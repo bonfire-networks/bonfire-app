693589916ab1e63cb7379caf90674a62a7d88147
bernini <bernini@inventati.org>
2020-09-08 21:11:43 +0200

    main theme and login/signup pages

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -35,6 +35,19 @@ module.exports = (env, options) => {
           }
         },
         {
+          // fonts
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "../fonts",
+              },
+            },
+          ]
+        },
+        {
           test: /\.[s]?css$/,
           use: [
             MiniCssExtractPlugin.loader,
