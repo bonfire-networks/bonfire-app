ed4b61739abcd10714bf33e8a2de7fd8bd1812f0
jjl <james.laver@gmail.com>
2020-11-22 21:13:27 +0000

    remove duplicate live plugs

--- lib/web/live_plugs/static_changed.ex
+++
@@ -1,7 +0,0 @@
-defmodule Bonfire.Common.Web.LivePlugs.StaticChanged do
-
-  import Phoenix.LiveView
-
-  def mount(_, _, socket), do: {:ok, assign(socket, :static_changed, static_changed?(socket))}
-
-end
