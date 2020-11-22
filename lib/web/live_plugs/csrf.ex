b995fd045510d0ddd95ba2a501d7cd1b014ad33b
jjl <james.laver@gmail.com>
2020-11-22 21:13:27 +0000

    remove duplicate live plugs

--- lib/web/live_plugs/csrf.ex
+++
@@ -1,8 +0,0 @@
-defmodule Bonfire.Common.Web.LivePlugs.Csrf do
-
-  import Phoenix.LiveView
-
-  def mount(_, %{"_csrf_token" => token}, socket), do: {:ok, assign(socket, :csrf_token, token)}
-  def mount(_, _, socket), do: {:ok, socket}
-
-end
