d62c123ae469c81337d20bdb2aeba7361f667a4b
jjl <james.laver@gmail.com>
2020-11-22 16:47:22 +0000

    signup, confirm email, login flow

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -22,27 +22,35 @@ defmodule Bonfire.Web.Router do
     pipe_through :browser
     # guest visible pages
     live "/", HomeLive, :home
+  end
 
+  scope "/", Bonfire.Me.Web do
+    pipe_through :browser
+    resources "/login", LoginController, only: [:index, :create]
+    resources "/confirm-email", ConfirmEmailController, only: [:index, :create, :show]
+    resources "/signup", SignupController, only: [:index, :create]
+  end
+
+  scope "/", Bonfire.Web do
+    pipe_through :browser
     # user-only pages
-    live "/home", HomeLive, :home
-    live "/home/@:username", HomeLive, :home_user
-
+    live "/~", HomeLive, :home
+    live "/~@:username", HomeLive, :home_user
   end
 
   # include federation routes
   use ActivityPubWeb.Router
 
-  # include routes from CommonsPub extensions
-  use Bonfire.Me.Web.Router
-
-
   if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
     scope "/" do
       pipe_through :browser
-      live_dashboard "/dashboard", metrics: Bonfire.Web.Telemetry
-      forward "/emails", Bamboo.SentEmailViewerPlug
+      if Code.ensure_loaded?(Phoenix.LiveDashboard.Router) do
+        import Phoenix.LiveDashboard.Router
+        live_dashboard "/dashboard", metrics: Bonfire.Web.Telemetry
+      end
+      if Code.ensure_loaded?(Bamboo.SentEmailViewerPlug) do
+        forward "/emails", Bamboo.SentEmailViewerPlug
+      end
     end
   end
 end
