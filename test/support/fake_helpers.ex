d63103b7096f0424449bf47201a2b2c140fbd527
Mayel <git@mayel.space>
2020-12-01 10:54:08 +0100

    fix tests

--- test/support/fake_helpers.ex
+++ test/support/fake_helpers.ex
@@ -1,19 +1,29 @@
 defmodule Bonfire.Test.FakeHelpers do
 
   alias Bonfire.Data.Identity.Account
-  alias Bonfire.{Fake, Repo}
-  alias Bonfire.Me.{Accounts, Users}
+  alias Bonfire.Me.Fake
+  alias Bonfire.Me.Identity.{Accounts, Users}
+
   import ExUnit.Assertions
+
+  @repo Bonfire.Repo
 
   def fake_account!(attrs \\ %{}) do
     cs = Accounts.signup_changeset(Fake.account(attrs))
-    assert {:ok, account} = Repo.insert(cs)
+    assert {:ok, account} = @repo.insert(cs)
     account
   end
 
-  def fake_user!(%Account{}=account, attrs \\ %{}) do
+  def fake_user!(account \\ %{}, attrs \\ %{})
+
+  def fake_user!(%Account{}=account, attrs) do
     assert {:ok, user} = Users.create(Fake.user(attrs), account)
     user
   end
+
+  def fake_user!(account_attrs, user_attrs) do
+    fake_user!(fake_account!(account_attrs), user_attrs)
+  end
+
 
 end
