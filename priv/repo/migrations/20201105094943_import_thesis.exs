89552d5f976935a685e0d6c4be70a46094926b09
Karen Konou <konoukaren@gmail.com>
2020-12-07 09:58:39 +0100

    Fix migration

--- priv/repo/migrations/20201105094943_import_thesis.exs
+++ priv/repo/migrations/20201105094943_import_thesis.exs
@@ -2,6 +2,6 @@ defmodule Bonfire.PublisherThesis.Repo.Migrations.ImportThesis do
   use Ecto.Migration
 
   def change do
-    if Code.is_loaded?(Bonfire.PublisherThesis.Migration), do: Bonfire.PublisherThesis.Migration.migrate_thesis
+    if Code.ensure_loaded?(Bonfire.PublisherThesis.Migration), do: Bonfire.PublisherThesis.Migration.migrate_thesis
   end
 end
