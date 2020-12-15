b5705a2a56ea28a1ceb39f2465f3ebab29a223f7
Karen Konou <konoukaren@gmail.com>
2020-12-15 18:07:02 +0100

    Update posts test

--- test/posts_test.exs
+++ test/posts_test.exs
@@ -7,12 +7,12 @@ defmodule Bonfire.Me.PostsTest do
   alias Bonfire.Me.Fake
 
   test "creation works" do
-    attrs = %{summary: "summary", name: "name", html_content: "<p>epic html message</p>"}
+    attrs = %{summary: "summary", name: "name", html_body: "<p>epic html message</p>"}
     assert {:ok, account} = Accounts.signup(Fake.account())
     user_attrs = Fake.user()
     assert {:ok, user} = Users.create(user_attrs, account)
     assert {:ok, post} = Posts.create(user, attrs)
-    assert post.post_content.html_content == "<p>epic html message</p>"
+    assert post.post_content.html_body == "<p>epic html message</p>"
     assert post.post_content.name == "name"
     assert post.post_content.summary == "summary"
     assert post.created.creator_id == user.id
