f77fa9a7d9177e8be8479cf82c11ccc624cbab2c
Karen Konou <konoukaren@gmail.com>
2020-12-15 11:16:43 +0100

    Update for creator

--- test/posts_test.exs
+++ test/posts_test.exs
@@ -2,12 +2,19 @@ defmodule Bonfire.Me.PostsTest do
   use Bonfire.DataCase
 
   alias Bonfire.Me.Social.Posts
+  alias Bonfire.Me.Identity.Accounts
+  alias Bonfire.Me.Identity.Users
+  alias Bonfire.Me.Fake
 
   test "creation works" do
     attrs = %{summary: "summary", name: "name", html_content: "<p>epic html message</p>"}
-    assert {:ok, post} = Posts.create(nil, attrs)
+    assert {:ok, account} = Accounts.signup(Fake.account())
+    user_attrs = Fake.user()
+    assert {:ok, user} = Users.create(user_attrs, account)
+    assert {:ok, post} = Posts.create(user, attrs)
     assert post.post_content.html_content == "<p>epic html message</p>"
     assert post.post_content.name == "name"
     assert post.post_content.summary == "summary"
+    assert post.created.creator_id == user.id
   end
 end
