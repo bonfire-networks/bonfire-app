b6a38a1cb6e1e171b2ab72e6ac06e75dd76cea66
jjl <james.laver@gmail.com>
2020-12-22 16:56:02 +0000

    fix login and stuff

--- lib/web/plugs/load_current_user.ex
+++ lib/web/plugs/load_current_user.ex
@@ -1,15 +1,13 @@
 defmodule Bonfire.Web.Plugs.LoadCurrentUser do
 
-  import Plug.Conn
+  use Bonfire.Web, :plug
   alias Bonfire.Me.Identity.Users
   alias Bonfire.Data.Identity.User
 
   def init(opts), do: opts
 
-  def call(conn, _opts), do: check(conn, get_session(conn, :user_id))
-
-  defp check(conn, id) when is_binary(id),
-    do: check(conn, Users.get_current(id, Map.get(conn.assigns, :current_account)))
-  defp check(conn, {:ok, me}), do: assign(conn, :current_user, me)
+  def call(conn, _opts) do
+    assign(conn, :current_user, Users.get_current(get_session(conn, :user_id)))
+  end
 
 end
