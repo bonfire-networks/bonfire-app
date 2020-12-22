a70832a77f500a8fe0f206273ab4d2b54ca7d6f1
jjl <james.laver@gmail.com>
2020-12-22 15:01:06 +0000

    merge

--- lib/web/plugs/account_required.ex
+++ lib/web/plugs/account_required.ex
@@ -2,25 +2,19 @@ defmodule Bonfire.Web.Plugs.AccountRequired do
 
   use Bonfire.Web, :plug
   alias Bonfire.Data.Identity.Account
+  alias Bonfire.Common.Web.Misc
 
   def init(opts), do: opts
 
   def call(conn, _opts), do: check(conn.assigns[:current_account], conn)
 
-  defp check(%Account{}, conn), do: conn #|> IO.inspect
+  defp check(%Account{}, conn), do: conn
   defp check(_, conn) do
     conn
     |> clear_session()
     |> put_flash(:error, "You need to log in to view that page.")
-    |> go(Routes.login_path(conn, :index))
+    |> redirect(to: Routes.login_path(conn, :index) <> Misc.go_query(conn))
     |> halt()
   end
-
-  # TODO: should we preserve query strings?
-  defp go(conn, path) do
-    path = path <> "?" <> Query.encode(go: conn.requested_path)
-    redirect(conn, to: path)
-  end
-
 
 end
