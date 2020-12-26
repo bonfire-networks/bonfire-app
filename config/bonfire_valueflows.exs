493f2970c984d79b36b463b9cfd5af9a3bfa5ee3
Mayel <git@mayel.space>
2020-12-26 18:01:34 +0100

    prepare for teating

--- config/bonfire_valueflows.exs
+++ config/bonfire_valueflows.exs
@@ -3,6 +3,8 @@ use Mix.Config
 config :bonfire_valueflows,
   web_module: Bonfire.Web,
   repo_module: Bonfire.Repo,
+  endpoint_module: Bonfire.Web.Endpoint,
+  graphql_schema_module: Bonfire.GraphQL.Schema,
   user_schema: Bonfire.Data.Identity.User,
   org_schema: Bonfire.Data.Identity.User,
   valid_agent_schemas: [Bonfire.Data.Identity.User],
--- mix.exs
+++ mix.exs
@@ -3,44 +3,9 @@ defmodule Bonfire.MixProject do
 
   use Mix.Project
 
-  def project do
-    [
-      app: :bonfire,
-      version: "0.1.0",
-      elixir: "~> 1.11",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
-      start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
-      deps: Mess.deps [
-        ## password hashing - builtin vs nif
-        {:pbkdf2_elixir, "~> 1.2.1", only: [:dev, :test]},
-        {:argon2_elixir, "~> 2.3.0", only: [:prod]},
-        ## dev conveniences
-        {:dbg, "~> 1.0", only: [:dev, :test]},
-        {:phoenix_live_reload, "~> 1.2", only: :dev},
-        {:exsync, "~> 0.2", only: :dev},
-        # tests
-        {:floki, ">= 0.0.0", only: [:dev, :test]},
-      ]
-    ]
-  end
-
-  def application do
-    [
-      mod: {Bonfire.Application, []},
-      extra_applications: [:logger, :runtime_tools, :ssl, :bamboo, :bamboo_smtp]
-    ]
-  end
-
-  defp deps() do
-  end
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
-
   @bonfire_deps [
     "pointers",
+    "activity_pub",
     "bonfire_common",
     "bonfire_data_access_control",
     "bonfire_data_identity",
@@ -50,7 +15,55 @@ defmodule Bonfire.MixProject do
     "bonfire_geolocate",
     "bonfire_quantify",
     "bonfire_valueflows",
-  ] |> Enum.join(" ")
+  ]
+
+  def project do
+    [
+      app: :bonfire,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_paths: ["test"] ++ existing_dep_paths(@bonfire_deps, "test"),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+
+  defp deps() do
+    Mess.deps([
+      ## password hashing - builtin vs nif
+      {:pbkdf2_elixir, "~> 1.2.1", only: [:dev, :test]},
+      {:argon2_elixir, "~> 2.3.0", only: [:prod]},
+      ## dev conveniences
+      {:dbg, "~> 1.0", only: [:dev, :test]},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:exsync, "~> 0.2", only: :dev},
+      # tests
+      {:floki, ">= 0.0.0", only: [:dev, :test]},
+      {:ex_machina, "~> 2.4", only: :test},
+      {:mock, "~> 0.3.0", only: :test},
+      {:zest, "~> 0.1", only: :test},
+    ])
+    # |> IO.inspect()
+  end
+
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"] ++ existing_dep_paths(@bonfire_deps, "test/support")
+  defp elixirc_paths(_), do: ["lib"]
+
+
+  def application do
+    [
+      mod: {Bonfire.Application, []},
+      extra_applications: [:logger, :runtime_tools, :ssl, :bamboo, :bamboo_smtp]
+    ]
+  end
+
+  @bonfire_deps_str @bonfire_deps |> Enum.join(" ")
 
   defp aliases do
     [
@@ -59,8 +72,8 @@ defmodule Bonfire.MixProject do
       "js.deps.get": ["cmd npm install --prefix assets"],
       "js.deps.update": ["cmd npm update --prefix assets"],
       "ecto.seeds": ["run priv/repo/seeds.exs"],
-      "bonfire.deps.update": ["deps.update #{@bonfire_deps}"],
-      "bonfire.deps.clean": ["deps.clean #{@bonfire_deps} --build"],
+      "bonfire.deps.update": ["deps.update #{@bonfire_deps_str}"],
+      "bonfire.deps.clean": ["deps.clean #{@bonfire_deps_str} --build"],
       "bonfire.deps": ["bonfire.deps.update", "bonfire.deps.clean"],
       setup: ["hex.setup", "rebar.setup", "deps.get", "bonfire.deps.clean", "ecto.setup", "js.deps.get"],
       updates: ["deps.get", "bonfire.deps", "ecto.migrate", "js.deps.get"],
@@ -68,6 +81,21 @@ defmodule Bonfire.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
+  end
+
+  defp dep_path(dep) do
+    # use locally cloned repo if path defined and active, otherwise stick to code obtained by mix deps.get
+    deps()[String.to_existing_atom(dep)][:path] || "./deps/"<>dep
+  end
+
+  defp existing_dep_paths(list, path) do
+    Enum.map(list, fn dep -> dep_path(dep) <>"/"<>path end)
+    |> existing_paths()
+    # |> IO.inspect()
+  end
+
+  defp existing_paths(list) do
+    Enum.filter(list, &File.exists?(&1))
   end
 
 end
