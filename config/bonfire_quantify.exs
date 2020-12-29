0968a03af43fca57294cb874ee48e3776d1b781d
Mayel <git@mayel.space>
2020-12-29 12:31:27 +0100

    geo + quant testing

--- config/bonfire_quantify.exs
+++ config/bonfire_quantify.exs
@@ -2,7 +2,10 @@ use Mix.Config
 
 config :bonfire_quantify,
   web_module: Bonfire.Web,
+  endpoint_module: Bonfire.Web.Endpoint,
   repo_module: Bonfire.Repo,
   user_schema: Bonfire.Data.Identity.User,
   # templates_path: "lib",
   otp_app: :bonfire
+
+config :bonfire_quantify, Bonfire.Quantify.Units, valid_contexts: [Bonfire.Quantify.Units, Bonfire.Data.Identity.User]
