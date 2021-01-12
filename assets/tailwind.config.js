5d94e5b845a057d784eed8b636b0e38d79e1af2e
bernini <bernini@inventati.org>
2021-01-12 17:06:58 +0100

    tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -15,14 +15,21 @@ module.exports = {
       colors: {
         blueGray: colors.blueGray,
         amber: colors.amber,
-        rose: colors.rose
+        rose: colors.rose,
+        orange: colors.orange,
+        teal: colors.teal,
+        cyan: colors.cyan
       },
       boxShadow: {
         tick: '-3px 3px 1px 0 rgba(0,0,0,.1)'
       }
     },
   },
-  variants: {},
+  variants: {
+    extend: {
+     borderWidth: ['hover', 'focus'],
+    }
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
