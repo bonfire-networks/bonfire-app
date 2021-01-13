76d16ded4c0db27f659f4177c3b0858aa085eddb
Mayel <git@mayel.space>
2021-01-13 22:20:43 +0100

    remove extensions

--- priv/repo/migrations/20210102094944_import_shared_user.exs
+++
@@ -1,9 +0,0 @@
-defmodule Bonfire.Repo.Migrations.ImportSharedUser do
-  use Ecto.Migration
-
-  import Bonfire.Data.SharedUser.Migration
-  # accounts & users
-
-  def change, do: migrate_shared_user()
-
-end
