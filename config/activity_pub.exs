e2901927244241b6e83ad62e729342e338c27246
Karen Konou <konoukaren@gmail.com>
2021-01-20 11:37:06 +0100

    Import default AP config

--- config/activity_pub.exs
+++ config/activity_pub.exs
@@ -7,3 +7,30 @@ config :nodeinfo, :adapter, Bonfire.NodeinfoAdapter
 
 config :bonfire, Bonfire.ActivityPub.Adapter,
   users_module: Bonfire.Me.Identity.Users.ActivityPub
+
+config :activity_pub, :mrf_simple,
+  media_removal: [],
+  media_nsfw: [],
+  report_removal: [],
+  accept: [],
+  avatar_removal: [],
+  banner_removal: []
+
+config :activity_pub, :instance,
+  hostname: "localhost",
+  federation_publisher_modules: [ActivityPubWeb.Publisher],
+  federation_reachability_timeout_days: 7,
+  federating: true,
+  rewrite_policy: []
+
+config :activity_pub, :http,
+  proxy_url: nil,
+  send_user_agent: true,
+  adapter: [
+    ssl_options: [
+      # Workaround for remote server certificate chain issues
+      partial_chain: &:hackney_connect.partial_chain/1,
+      # We don't support TLS v1.3 yet
+      versions: [:tlsv1, :"tlsv1.1", :"tlsv1.2"]
+    ]
+  ]
