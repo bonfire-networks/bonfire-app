56bd7dd8a6420eb562296fc34a3c8ffa162b8519
Mayel <git@mayel.space>
2021-03-11 17:00:56 +0100

    switch admin to user

--- lib/web/plugs/admin_required.ex
+++ lib/web/plugs/admin_required.ex
@@ -1,15 +1,15 @@
 defmodule Bonfire.Web.Plugs.AdminRequired do
 
   use Bonfire.Web, :plug
-  alias Bonfire.Data.Identity.Account
+  alias Bonfire.Data.Identity.User
   alias Bonfire.Web.HomeLive
   alias Bonfire.Me.Web
 
   def init(opts), do: opts
 
-  def call(conn, _opts), do: check(conn.assigns[:current_account], conn)
+  def call(conn, _opts), do: check(conn.assigns[:current_user], conn)
 
-  defp check(%Account{instance_admin: %{is_instance_admin: true}}, conn), do: conn
+  defp check(%User{instance_admin: %{is_instance_admin: true}}, conn), do: conn
   defp check(_, conn) do
     conn
     |> clear_session()
