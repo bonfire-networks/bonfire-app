0f5acd5832f7e970606744d7ff900fc72c9ab7ee
Mayel <git@mayel.space>
2021-04-01 15:18:36 +0200

    count replies

--- priv/repo/migrations/20210401094942_count_functions.exs
+++ priv/repo/migrations/20210401094942_count_functions.exs
@@ -4,11 +4,16 @@ defmodule Bonfire.Social.Repo.Migrations.CountFunctions do
   import Bonfire.Data.Social.FollowCount.Migration
   import Bonfire.Data.Social.BoostCount.Migration
   import Bonfire.Data.Social.LikeCount.Migration
+  import Bonfire.Data.Social.Replied.Migration
 
   def change do
     Bonfire.Data.Social.FollowCount.Migration.migrate_functions()
     Bonfire.Data.Social.BoostCount.Migration.migrate_functions()
     Bonfire.Data.Social.LikeCount.Migration.migrate_functions()
+
+    Bonfire.Data.Social.Replied.Migration.add_count_fields()
+    # flush()
+    Bonfire.Data.Social.Replied.Migration.migrate_functions()
   end
 
 end
