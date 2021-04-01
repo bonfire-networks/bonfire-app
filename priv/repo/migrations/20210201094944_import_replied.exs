05c8c4cde7ec623ccdb1db22cbfd8c13f650cfd7
Mayel <git@mayel.space>
2021-04-01 15:55:19 +0200

    fix migrations

--- priv/repo/migrations/20210201094944_import_replied.exs
+++ priv/repo/migrations/20210201094944_import_replied.exs
@@ -1,9 +1,13 @@
-defmodule Bonfire.Repo.Migrations.ImportMeMore do
+defmodule Bonfire.Repo.Migrations.ImportReplied do
   use Ecto.Migration
 
   import Bonfire.Data.Social.Replied.Migration
 
-  def up, do: migrate_replied(:up)
+  def up do
+    migrate_replied(:up)
+    migrate_functions()
+  end
+
   def down, do: migrate_replied(:down)
 
 end
