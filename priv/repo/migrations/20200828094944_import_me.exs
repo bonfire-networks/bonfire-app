4984ef5d09ccb6f62153727acbcb9a09eef47c44
Mayel de Borniol <115318+mayel@users.noreply.github.com>
2021-04-20 14:17:22 +0200

    Update 20200828094944_import_me.exs

--- priv/repo/migrations/20200828094944_import_me.exs
+++ priv/repo/migrations/20200828094944_import_me.exs
@@ -7,13 +7,6 @@ defmodule Bonfire.Repo.Migrations.ImportMe do
   def up do
     # accounts & users
     migrate_me()
-
-    flush()
-
-    alter table("bonfire_data_social_profile") do
-      Ecto.Migration.add_if_not_exists :icon_id, strong_pointer(Bonfire.Files.Media)
-      Ecto.Migration.add_if_not_exists :image_id, strong_pointer(Bonfire.Files.Media)
-    end
   end
 
   def down, do: migrate_me()
