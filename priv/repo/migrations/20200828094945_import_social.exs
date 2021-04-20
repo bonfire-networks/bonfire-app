d7cbce37f411a4755db0cc61b24368b02106fd62
Mayel de Borniol <115318+mayel@users.noreply.github.com>
2021-04-20 14:17:53 +0200

    Update 20200828094945_import_social.exs

--- priv/repo/migrations/20200828094945_import_social.exs
+++ priv/repo/migrations/20200828094945_import_social.exs
@@ -3,7 +3,18 @@ defmodule Bonfire.Social.Repo.Migrations.ImportSocial do
 
   import Bonfire.Social.Migrations
 
-  def up, do: migrate_social()
+  def up do 
+  
+    migrate_social()
+  
+    flush()
+
+    alter table("bonfire_data_social_profile") do
+      Ecto.Migration.add_if_not_exists :icon_id, strong_pointer(Bonfire.Files.Media)
+      Ecto.Migration.add_if_not_exists :image_id, strong_pointer(Bonfire.Files.Media)
+    end
+  end
+  
   def down, do: migrate_social()
 
 end
