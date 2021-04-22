047b36f05f1fcc91e621c5c250d091cc9e1c617d
ivan <bernini@inventati.org>
2021-04-22 18:45:24 +0200

    bonfire config

--- config/bonfire_ui.exs
+++ config/bonfire_ui.exs
@@ -4,4 +4,19 @@ config :bonfire, :ui,
    sidebar_components: [
       Bonfire.UI.Social.SidebarNavigationLive,
       Bonfire.UI.Reflow.SidebarNavigationLive
-    ]
+   ],
+   profile_navigation: [
+      timeline: "timeline",
+      posts: "posts",
+      boosts: "boosts",
+      inventory: "inventory"
+   ],
+   profile_sections: [
+      private: Bonfire.UI.Social.PrivateLive,
+      timeline: Bonfire.UI.Social.ProfileTimelineLive,
+      posts: Bonfire.UI.Social.ProfilePostsLive,
+      boosts: Bonfire.UI.Social.ProfileBoostsLive,
+      followers: Bonfire.UI.Social.ProfileFollowersLive,
+      followed: Bonfire.UI.Social.ProfileFollowersLive,
+      inventory: Bonfire.UI.Reflow.ProfileInventoryLive,
+   ]
