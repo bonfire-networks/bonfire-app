89789136b7e14d59a1cdd8a3c4eac1cc9793575e
ivan <bernini@inventati.org>
2021-04-27 12:46:36 +0200

    fixed mix.exs dep

--- mix.exs
+++ mix.exs
@@ -71,14 +71,15 @@ defmodule Bonfire.MixProject do
       {:ex_machina, "~> 2.4", only: :test},
       {:mock, "~> 0.3.0", only: :test},
       {:zest, "~> 0.1"},
-      {:grumble, "~> 0.1.3", only: [:test]}
+      {:grumble, "~> 0.1.3", only: [:test]},
 
       # list dependencies & licenses
       {:licensir, only: :dev, runtime: false,
         git: "https://github.com/mayel/licensir", branch: "pr",
         # path: "./forks/licensir"
       },
-
+      # component library
+      {:surface_catalogue, "~> 0.0.7", only: [:dev]},
       # security auditing
       # {:mix_audit, "~> 0.1", only: [:dev], runtime: false}
       {:sobelow, "~> 0.8", only: :dev}
