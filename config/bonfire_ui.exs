ec52be42163674313db47c337c233ec8c243ce14
ivan <bernini@inventati.org>
2021-04-29 18:17:06 +0200

    up config ui

--- config/bonfire_ui.exs
+++ config/bonfire_ui.exs
@@ -5,64 +5,55 @@ config :bonfire, :ui,
       Bonfire.UI.Social.SidebarNavigationLive,
       Bonfire.UI.ValueFlows.ProcessesListLive
    ],
-   profile_sections: [
-      timeline: Bonfire.UI.Social.ProfileTimelineLive,
-      private: Bonfire.UI.Social.PrivateLive,
-      posts: Bonfire.UI.Social.ProfilePostsLive,
-      boosts: Bonfire.UI.Social.ProfileBoostsLive,
-      followers: Bonfire.UI.Social.ProfileFollowersLive,
-      followed: Bonfire.UI.Social.ProfileFollowersLive,
-      inventory: Bonfire.UI.Reflow.ProfileInventoryLive,
-   ],
-   profile_navigation: [
-      timeline: "timeline",
-      inventory: "inventory",
-   ],
-   profile_widgets: [
-      Bonfire.UI.Social.SearchWidgetLive
-   ],
-   resource_navigation: [
-      timeline: "timeline",
-      material_passport: "material passport",
-   ],
-   resource_widgets: [
-      Bonfire.UI.Social.SearchWidgetLive,
-     # Bonfire.UI.Social.SubscribeWidgetLive,
-      Bonfire.UI.ValueFlows.LocationWidgetLive,
-      Bonfire.UI.Social.HashtagsWidgetLive,
-   ],
-   process_navigation: [
-      events: "Economic events",
-      intents: "Intents",
-      # material_passport: "material passport",
-   ],
-   process_sections: [
-      events: Bonfire.UI.ValueFlows.EconomicEventsLive,
-      intents: Bonfire.UI.ValueFlows.IntentsLive,
-   ],
-   process_widgets: [
-      Bonfire.UI.Social.SearchWidgetLive,
-     # Bonfire.UI.Social.SubscribeWidgetLive,
-     # Bonfire.UI.ValueFlows.LocationWidgetLive,
-     # Bonfire.UI.Social.HashtagsWidgetLive,
-   ],
    smart_input: [
       post: true,
       cw: true,
       summary: true
    ],
-   smart_input_activities: [
-      #offer: "Publish an offer",
-      # need: "Publish a need",
-      # transfer_resource: "Transfer a resource",
-      # produce_resource: "Add a resource",
-      # process: "Create a process"
-   ],
-   smart_input_forms: [
-      post: Bonfire.UI.Social.CreateActivityLive,
-      #offer: Bonfire.UI.ValueFlows.CreateOfferWidgetLive
-      # need:
-      transfer_resource: Bonfire.UI.ValueFlows.CreateOfferWidgetLive,
-      produce_resource: Bonfire.UI.ValueFlows.CreateOfferWidgetLive,
-      process: Bonfire.UI.ValueFlows.CreateOfferWidgetLive
+   profile: [
+      sections: [
+         timeline: Bonfire.UI.Social.ProfileTimelineLive,
+         private: Bonfire.UI.Social.PrivateLive,
+         posts: Bonfire.UI.Social.ProfilePostsLive,
+         boosts: Bonfire.UI.Social.ProfileBoostsLive,
+         followers: Bonfire.UI.Social.ProfileFollowersLive,
+         followed: Bonfire.UI.Social.ProfileFollowersLive,
+         inventory: Bonfire.UI.Reflow.ProfileInventoryLive,
+      ],
+      navigation: [
+         timeline: "timeline",
+         inventory: "inventory",
+      ],
+      widgets: [
+         Bonfire.UI.Social.SearchWidgetLive
+      ],
    ]
+   resource: [
+      navigation: [
+         timeline: "timeline",
+         material_passport: "material passport",
+      ],
+      widgets: [
+         Bonfire.UI.Social.SearchWidgetLive,
+        # Bonfire.UI.Social.SubscribeWidgetLive,
+         Bonfire.UI.ValueFlows.LocationWidgetLive,
+         Bonfire.UI.Social.HashtagsWidgetLive,
+      ],
+   ],
+   # process: [
+   #    navigation: [
+   #       events: "Economic events",
+   #       intents: "Intents",
+   #       # material_passport: "material passport",
+   #    ],
+   #    sections: [
+   #       events: Bonfire.UI.ValueFlows.EconomicEventsLive,
+   #       intents: Bonfire.UI.ValueFlows.IntentsLive,
+   #    ],
+   #    widgets: [
+   #       Bonfire.UI.Social.SearchWidgetLive,
+   #      # Bonfire.UI.Social.SubscribeWidgetLive,
+   #      # Bonfire.UI.ValueFlows.LocationWidgetLive,
+   #      # Bonfire.UI.Social.HashtagsWidgetLive,
+   #    ],
+   # ]
