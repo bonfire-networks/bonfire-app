2bddee3f4c9b6c39a42ec88a3b922577f1ed1c7e
Mayel <git@mayel.space>
2021-05-01 14:57:01 +0200

    version

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Bonfire.MixProject do
   def project do
     [
       app: :bonfire,
-      version: "0.1.0-alpha.41",
+      version: "0.1.0-alpha-valueflows.42",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_paths: ["test"] ++ existing_deps_paths(@bonfire_test_deps, "test"),
