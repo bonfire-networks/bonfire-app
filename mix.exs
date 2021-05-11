1afe09afbf86021e0d321132ec1c6d0686c01441
Mayel <git@mayel.space>
2021-05-11 15:31:15 +0200

    remove mix tasks, fix mix

--- mix.exs
+++ mix.exs
@@ -44,6 +44,7 @@ defmodule Bonfire.MixProject do
       {:mock, "~> 0.3.0", only: :test},
       {:zest, "~> 0.1"},
       {:grumble, "~> 0.1.3", only: [:test], override: true},
+      {:bonfire_api_graphql, git: "https://github.com/bonfire-networks/bonfire_api_graphql", branch: "main", only: [:test]},
 
       # list dependencies & licenses
       {:licensir, only: :dev, runtime: false,
@@ -57,8 +58,9 @@ defmodule Bonfire.MixProject do
       # security auditing
       # {:mix_audit, "~> 0.1", only: [:dev], runtime: false}
       {:sobelow, "~> 0.8", only: :dev}
-    ])
-    # |> IO.inspect()
+      ]
+    )
+    |> IO.inspect()
   end
 
   defp deps(test) when is_atom(test), do: deps(&dep?(test, &1))
@@ -77,6 +79,8 @@ defmodule Bonfire.MixProject do
         # "phil_columns.seed",
         "run #{flavour_path()}/repo/seeds.exs"
         ],
+      "bonfire.deps.update": ["deps.update " <>deps_to_update()],
+      "bonfire.deps.clean": ["deps.clean " <>deps_to_clean()<>" --build"],
       "bonfire.deps": ["bonfire.deps.update", "bonfire.deps.clean"],
       setup: ["hex.setup", "rebar.setup", "deps.get", "bonfire.deps.clean", "ecto.setup", "js.deps.get"],
       updates: ["deps.get", "bonfire.deps", "js.deps.get"],
@@ -87,9 +91,9 @@ defmodule Bonfire.MixProject do
     ]
   end
 
-  defp flavour_path(), do: System.get_env("BONFIRE_FLAVOUR", "flavours/"<>System.get_env("FLAVOUR", "classic"))
+  def flavour_path(), do: System.get_env("BONFIRE_FLAVOUR", "flavours/"<>System.get_env("FLAVOUR", "classic"))
 
-  defp config_path(flavour_path \\ flavour_path(), filename),
+  def config_path(flavour_path \\ flavour_path(), filename),
     do: Path.expand(Path.join([flavour_path, "config", filename]))
 
   defp mess_sources() do
@@ -99,6 +103,28 @@ defmodule Bonfire.MixProject do
 
   defp mess_sources("0"), do: [git: "deps.git", hex: "deps.hex"]
   defp mess_sources(_),   do: [path: "deps.path", git: "deps.git", hex: "deps.hex"]
+
+  def deps_to_clean() do
+      deps()
+      |> Enum.map(&dep_name/1)
+      |> Enum.filter(&data_dep?/1)
+      |> Enum.join(" ")
+  end
+
+  def deps_to_update() do
+    deps()
+    |> Enum.filter(&should_update_dep?/1)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.join(" ")
+    |> IO.inspect(label: "You can run `mix bonfire.deps.update` to update these extensions and dependencies")
+  end
+
+  defp should_update_dep?(dep), do: unpinned_git_dep?(dep)
+
+  defp unpinned_git_dep?(dep) do
+    spec = elem(dep, 1)
+    is_list(spec) && spec[:git] && !spec[:commit]
+  end
 
   defp dep_path(dep) do
     spec = elem(dep, 1)
@@ -123,6 +149,8 @@ defmodule Bonfire.MixProject do
   defp dep?(:test, dep), do: elem(dep, 0) in @test_deps || String.starts_with?(dep_name(dep), "bonfire_")
 
   defp dep_name(dep), do: Atom.to_string(elem(dep, 0))
+
+  def data_dep?(dep), do: String.starts_with?(dep, "bonfire_data_")
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: test_lib_paths()
