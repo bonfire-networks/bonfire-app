c1d4eff32898020738e7bf0f182e2d7467cf5937
Mayel <git@mayel.space>
2021-05-14 10:58:32 +0200

    up

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bonfire.MixProject do
   def project do
     [
       app: :bonfire,
-      version: "0.1.0-alpha.87",
+      version: "0.1.0-alpha.88",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()) |> IO.inspect,
       test_paths: test_paths(),
