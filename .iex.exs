684313fbf3f97bafc152ddb401b1a12e4b130d4d
Mayel <git@mayel.space>
2021-05-18 13:25:48 +0200

    refactor paths

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,11 @@
 # if Code.ensure_loaded?(ExSync) && function_exported?(ExSync, :register_group_leader, 0) do
 #   ExSync.register_group_leader()
 # end
+
+alias Bonfire.Repo
+alias Bonfire.Data
+alias Bonfire.Me
+alias Bonfire.Social
+alias Bonfire.Common
+import Common.Utils
+import Common.URIs
