b7e3f6ffc5fb14f0e35bff2c67ab208f15dbab46
Mayel <git@mayel.space>
2021-06-25 14:12:33 +0200

    up

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bonfire.MixProject do
   use Mix.Project
 
   @config [
-      version: "0.1.0-alpha.175", # note that the flavour will automatically be added where the dash appears
+      version: "0.1.0-alpha.176", # note that the flavour will automatically be added where the dash appears
       elixir: "~> 1.11",
       default_flavour: "classic",
       test_deps_prefixes: ["bonfire_", "pointers"],
