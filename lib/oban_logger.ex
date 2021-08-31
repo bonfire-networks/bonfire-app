29fb91ce4d18402d6019efff7210b73e1663e25d
Karen Konou <konoukaren@gmail.com>
2021-08-31 11:37:35 +0200

    Try to fix oban logger

--- lib/oban_logger.ex
+++ lib/oban_logger.ex
@@ -1,8 +1,8 @@
 defmodule Bonfire.ObanLogger do
   def handle_event([:oban, :job, :exception], measure, meta, _) do
     extra =
-      meta.job
-      |> Map.take([:id, :args, :meta, :queue, :worker])
+      meta
+      |> Map.take([:id, :args, :queue, :worker])
       |> Map.merge(measure)
 
     Sentry.capture_exception(meta.error, stacktrace: meta.stacktrace, extra: extra)
