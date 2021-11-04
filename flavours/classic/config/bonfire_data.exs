a8da4397a4c549c77259e4f7d871d3420b479777
Mayel <git@mayel.space>
2021-11-05 11:02:21 +1300

    config

--- flavours/classic/config/bonfire_data.exs
+++ flavours/classic/config/bonfire_data.exs
@@ -369,10 +369,6 @@ config :bonfire_geolocate, Bonfire.Geolocate.Geolocation,
   ]
 
 
-# all data types included in federation by default (excluding ValueFlows)
-config :bonfire, :all_types, [User, Post, Measure, Unit, Geolocation, Category, Message]
-
-
 config :bonfire_files, Bonfire.Files.Media,
   field: [
     url: {:string, virtual: true}
