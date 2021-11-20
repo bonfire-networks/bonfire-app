49d3e1f43eec230267797dd204122652d4aa454f
Mayel de Borniol <git@mayel.space>
2021-11-20 14:21:40 +1300

    misc

--- lib/oban_logger.ex
+++ lib/oban_logger.ex
@@ -1,9 +1,13 @@
 defmodule Bonfire.ObanLogger do
+  require Logger
+
   def handle_event([:oban, :job, :exception], measure, meta, _) do
     extra =
       meta.job
       |> Map.take([:id, :args, :meta, :queue, :worker])
       |> Map.merge(measure)
+
+    Logger.error(meta.error)
 
     Sentry.capture_exception(meta.error, stacktrace: meta.stacktrace, extra: extra)
   end
