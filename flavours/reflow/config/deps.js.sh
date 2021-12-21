ece185d72cb685822bb56bbc8f92047b9cc895f3
Mayel de Borniol <git@mayel.space>
2021-12-21 19:47:44 +1300

    consolidate js.get script

--- flavours/reflow/config/deps.js.sh
+++ flavours/reflow/config/deps.js.sh
@@ -3,12 +3,4 @@
 # add more modules separated by $IFS
 DEPS='bonfire_geolocate'
 
-for dep in $DEPS ; do
-	printf "Install JS deps from '%s'\n" "$dep"
-	if cd "forks/$dep/assets" 2>/dev/null || cd "deps/$dep/assets" 2>/dev/null ; then
-		pnpm install
-		cd ../../../
-	else
-		printf "Extension '%s' not available\n" "$dep"
-	fi
-done
+./priv/deps.js.sh "$DEPS"
