09720d722458b81ffe3dc6b69bc158a7c3786567
srfsh <srfsh@sonu.ch>
2021-12-21 09:24:54 +0300

    Improve deps.js.sh
    The script is now POSIX-compliant and more readable, plus doesn't warn when cd shell builtin can't cd into a directory.

--- flavours/reflow/config/deps.js.sh
+++ flavours/reflow/config/deps.js.sh
@@ -1,10 +1,14 @@
-#!/bin/bash 
+#!/bin/sh
 
-DEPS="bonfire_geolocate" 
+# add more modules separated by $IFS
+DEPS='bonfire_geolocate'
 
-for dep in $DEPS 
-do
- echo "Install JS deps from $dep"
- cd "forks/$dep/assets" && pnpm install || cd "deps/$dep/assets" && pnpm install || echo "Extension $dep not available"
- cd ../../../
+for dep in $DEPS ; do
+	printf "Install JS deps from '%s'\n" "$dep"
+	if cd "forks/$dep/assets" 2>/dev/null || cd "deps/$dep/assets" 2>/dev/null ; then
+		pnpm install
+		cd ../../../
+	else
+		printf "Extension '%s' not available\n" "$dep"
+	fi
 done
