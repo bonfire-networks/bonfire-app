da9c0440fc530c99aa04bc28173898df2ad10f41
Mayel de Borniol <git@mayel.space>
2021-12-22 20:08:08 +1300

    WIP language selector

--- flavours/cooperation/config/hooks.js
+++ flavours/cooperation/config/hooks.js
@@ -3,12 +3,13 @@ let ExtensionHooks = {};
 // TODO: make this more configurable? ie. don't import disabled extensions
 
 import { ThemeHooks } from "./../../../assets/js/theme"
+import { CommonHooks } from "./../../../forks/bonfire_common/assets/js/extension"
 import { InputSelectHooks } from "./../../../assets/js/input_select"
 import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
 import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
 import { NotificationsHooks } from "./../../../assets/js/notifications"
 
-Object.assign(ExtensionHooks, ThemeHooks, InputSelectHooks, GeolocateHooks, KanbanHooks, EditorCkHooks, NotificationsHooks)
+Object.assign(ExtensionHooks, ThemeHooks, InputSelectHooks, CommonHooks, GeolocateHooks, KanbanHooks, EditorCkHooks, NotificationsHooks)
  
 export { ExtensionHooks }
