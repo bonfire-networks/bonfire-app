e4d4ed0a48a16ab62e6ef214bf05e643c808f2bd
jjl <james.laver@gmail.com>
2022-01-09 11:19:16 +0100

    merge updates

--- .formatter.exs
+++
@@ -1,5 +0,0 @@
-[
-  import_deps: [:phoenix, :ecto_sql, :surface],
-  inputs: ["*.{ex,exs,sface}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,sface}", "forks/*/{config,lib,test}/**/*.{ex,exs,sface}"],
-  surface_inputs: ["{lib,test}/**/*.{ex,exs,sface}", "forks/*/{lib,test}/**/*.{ex,exs,sface}"],
-]
