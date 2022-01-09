d52867bb1aa4e549906e12bb7703bdfd10d5a8fa
jjl <james.laver@gmail.com>
2022-01-09 11:19:16 +0100

    merge updates

--- lib/web/router.ex
+++ lib/web/router.ex
@@ -102,28 +102,24 @@ defmodule Bonfire.Web.Router do
   scope "/", Bonfire.Me.Web do
     pipe_through :browser
     pipe_through :guest_only
-
   end
 
   # pages you need an account to view
   scope "/", Bonfire do
     pipe_through :browser
     pipe_through :account_required
-
  end
 
   # pages you need to view as a user
   scope "/", Bonfire do
     pipe_through :browser
     pipe_through :user_required
-
   end
 
   # pages only admins can view
   scope "/settings/admin" do
     pipe_through :browser
     pipe_through :admin_required
-
   end
 
 
