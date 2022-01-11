ed56a3dd7bc9f65656d43dcc976fc298b587e063
Mayel de Borniol <git@mayel.space>
2022-01-11 17:18:48 +1300

    mig

--- flavours/classic/repo/migrations/20200523081014_init_edges.exs
+++ flavours/classic/repo/migrations/20200523081014_init_edges.exs
@@ -1,18 +1,14 @@
 defmodule Bonfire.Repo.Migrations.InitEdges do
   use Ecto.Migration
-  alias Bonfire.Data.Edges.Edge.Migration, as: Edge
-  alias Bonfire.Data.Edges.EdgeTotal.Migration, as: EdgeTotal
-  require Edge
-  require EdgeTotal
+  alias Bonfire.Data.Edges.Migration
+
 
   def up do
-    Edge.migrate_edge()
-    EdgeTotal.migrate_edge_total()
+    Migration.up()
   end
 
   def down do
-    EdgeTotal.migrate_edge_total()
-    Edge.migrate_edge()
+    Migration.down()
   end
 
 end
