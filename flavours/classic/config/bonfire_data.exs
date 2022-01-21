1666cc1eb9ad8f11c727696d53b20eb3b0c6bc6a
Mayel de Borniol <git@mayel.space>
2022-01-22 11:04:26 +1300

    refactor

--- flavours/classic/config/bonfire_data.exs
+++ flavours/classic/config/bonfire_data.exs
@@ -58,7 +58,7 @@ alias Bonfire.Data.Identity.{
   Account, Accounted, Caretaker, Character, Credential, Email, Named, Self, Stereotype, User,
 }
 alias Bonfire.Data.Social.{
-  Activity, Article, Block, Bookmark, Created, Feed, FeedPublish, Inbox, Message, Follow,
+  Activity, Article, Block, Bookmark, Created, Feed, FeedPublish, Message, Follow,
   Boost, Like, Flag, Mention, Post, PostContent, Profile, Replied,
 }
 alias Bonfire.Classify.Category
@@ -173,7 +173,6 @@ config :bonfire_data_identity, Account,
   [code: quote do
     has_one :credential, unquote(Credential),foreign_key: :id
     has_one :email, unquote(Email), foreign_key: :id
-    has_one :inbox, unquote(Inbox), foreign_key: :id
     many_to_many :users, unquote(User),
       join_through: "bonfire_data_identity_accounted",
       join_keys: [account_id: :id, id: :id]
@@ -264,20 +263,9 @@ config :bonfire_data_social, Activity,
     # has_one:    [object_creator_profile: {[through: [:object_created, :creator_profile]]}],
     # ugly workaround needed for querying
     has_one :activity, unquote(Activity), foreign_key: :id, references: :id
-<<<<<<< HEAD
     has_one :like_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @like_ulid]
     has_one :boost_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @boost_ulid]
     has_one :follow_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @follow_ulid]
-    many_to_many :tags, Bonfire.Tag,
-      join_through: "bonfire_tagged", unique: true,
-      join_keys: [id: :object_id, tag_id: :id], on_replace: :delete
-=======
-    has_one :like_count, unquote(EdgeTotal), foreign_key: :id,
-      references: :id, where: [table_id: @like_ulid]
-    has_one :boost_count, unquote(EdgeTotal), foreign_key: :id,
-      references: :id, where: [table_id: @boost_ulid]
-    has_one :follow_count, unquote(EdgeTotal), foreign_key: :id,
-      references: :id, where: [table_id: @follow_ulid]
     has_many :controlled, unquote(Controlled), foreign_key: :id, references: :id
     has_many :tagged, unquote(Tagged), foreign_key: :id, references: :id
     has_many :feed_publishes, unquote(FeedPublish), references: :id, foreign_key: :activity_id
@@ -286,7 +274,6 @@ config :bonfire_data_social, Activity,
       unique: true,
       join_keys: [id: :object_id, tag_id: :id],
       on_replace: :delete
->>>>>>> 927ac65b5 (cleanup migrations)
   end]
 
 config :bonfire_data_social, Edge,
