86a896b9cb03303b599166927b9025bc74b70d12
jjl <james.laver@gmail.com>
2022-01-25 11:17:29 +0100

    config

--- flavours/classic/config/bonfire_data.exs
+++ flavours/classic/config/bonfire_data.exs
@@ -279,7 +279,6 @@ config :bonfire_data_social, Activity,
     has_one :like_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @like_ulid]
     has_one :boost_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @boost_ulid]
     has_one :follow_count, unquote(EdgeTotal), foreign_key: :id, references: :object_id, where: [table_id: @follow_ulid]
-    has_many :controlled, unquote(Controlled), foreign_key: :id, references: :id
     has_many :controlled, unquote(Controlled), foreign_key: :id, references: :object_id
     has_many :tagged, unquote(Tagged), foreign_key: :id, references: :object_id
     has_many :feed_publishes, unquote(FeedPublish), references: :id, foreign_key: :activity_id
