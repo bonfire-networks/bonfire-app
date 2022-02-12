907d9f752cf797e4e761a38e1e5d91c8deb94bb9
Mayel de Borniol <git@mayel.space>
2022-02-12 19:26:28 +1300

    fix

--- flavours/reflow/config/hooks.js
+++ flavours/reflow/config/hooks.js
@@ -2,8 +2,14 @@ let ExtensionHooks = {};
 
 // TODO: make this more configurable? ie. don't import disabled extensions
 
-// import { MyHooks } from "../../../deps/some_extension/assets/js/extension"
-import { GeolocateHooks } from "./../../../forks/bonfire_geolocate/assets/js/extension"
+import { ThemeHooks } from "./../../../assets/js/theme"
+import { CommonHooks } from "./../../../deps/bonfire_common/assets/js/extension"
+import { InputSelectHooks } from "./../../../assets/js/input_select"
+import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
+// import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
+import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
+import { NotificationsHooks } from "./../../../assets/js/notifications"
 
-Object.assign(ExtensionHooks, GeolocateHooks)
+Object.assign(ExtensionHooks, ThemeHooks, InputSelectHooks, CommonHooks, GeolocateHooks, EditorCkHooks, NotificationsHooks)
+
 export { ExtensionHooks }
