be37614307372be4ca23789e4b9607c40b8f9e05
Mayel de Borniol <git@mayel.space>
2022-02-12 19:26:28 +1300

    fix

--- flavours/cooperation/config/hooks.js
+++ flavours/cooperation/config/hooks.js
@@ -7,7 +7,7 @@ import { CommonHooks } from "./../../../deps/bonfire_common/assets/js/extension"
 import { InputSelectHooks } from "./../../../assets/js/input_select"
 import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
-import { EditorCkHooks } from "./../../../forks/bonfire_editor_ck/assets/js/extension"
+import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
 import { NotificationsHooks } from "./../../../assets/js/notifications"
 
 Object.assign(ExtensionHooks, ThemeHooks, InputSelectHooks, CommonHooks, GeolocateHooks, KanbanHooks, EditorCkHooks, NotificationsHooks)
--- flavours/haha/config/hooks.js
+++ flavours/haha/config/hooks.js
@@ -2,8 +2,12 @@ let ExtensionHooks = {};
 
 // TODO: make this more configurable? ie. don't import disabled extensions
 
-// import { MyHooks } from "../../../deps/some_extension/assets/js/extension"
+import { ThemeHooks } from "./../../../assets/js/theme"
+import { CommonHooks } from "./../../../deps/bonfire_common/assets/js/extension"
+import { InputSelectHooks } from "./../../../assets/js/input_select"
 import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
+import { NotificationsHooks } from "./../../../assets/js/notifications"
 
-Object.assign(ExtensionHooks, EditorCkHooks)
+Object.assign(ExtensionHooks, ThemeHooks, InputSelectHooks, CommonHooks, EditorCkHooks, NotificationsHooks)
+
 export { ExtensionHooks }
