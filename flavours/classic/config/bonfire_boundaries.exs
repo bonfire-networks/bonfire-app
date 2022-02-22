82db57484dba2496d50f86ef9a987a697a0605a1
jjl <james.laver@gmail.com>
2022-02-22 13:06:03 +0100

    update boundaries config

--- flavours/classic/config/bonfire_boundaries.exs
+++ flavours/classic/config/bonfire_boundaries.exs
@@ -20,44 +20,44 @@ verbs = %{
 
 config :bonfire,
   verbs: verbs,
+  create_verbs: [
+    block:  Bonfire.Data.Social.Block,
+    boost:  Bonfire.Data.Social.Boost,
+    follow: Bonfire.Data.Social.Follow,
+    flag:   Bonfire.Data.Social.Flag,
+    like:   Bonfire.Data.Social.Like,
+  ],
   circles: %{
-    guest:            %{id: "0AND0MSTRANGERS0FF1NTERNET", name: "Guests"},
-    local:            %{id: "3SERSFR0MY0VR10CA11NSTANCE", name: "Local Users"},
-    activity_pub:     %{id: "7EDERATEDW1THANACT1V1TYPVB", name: "ActivityPub Peers"},
+    guest:        %{id: "0AND0MSTRANGERS0FF1NTERNET", name: "Guests"},
+    local:        %{id: "3SERSFR0MY0VR10CA11NSTANCE", name: "Local Users"},
+    activity_pub: %{id: "7EDERATEDW1THANACT1V1TYPVB", name: "ActivityPub Peers"},
     # stereotypes
-    followers:        %{id: "7DAPE0P1E1PERM1TT0F0110WME", name: "My Followers"},
-    block:          %{id: "7N010NGERC0NSENTT0Y0VN0WTY", name: "Block"},
-    silence:          %{id: "7N010NGERWANTT011STENT0Y0V", name: "Silence"},
+    followers:    %{id: "7DAPE0P1E1PERM1TT0F0110WME", name: "My Followers"},
+    block:        %{id: "7N010NGERC0NSENTT0Y0VN0WTY", name: "Block"},
+    silence:      %{id: "7N010NGERWANTT011STENT0Y0V", name: "Silence"},
   },
   acls: %{
-    # read_only:           %{id: "AC10N1YACCESS1SREADACCESS1", name: "Read Only"},
-    # local:               %{id: "711M1TEDT010CA1VSERSS01E1Y", name: "Locally Public"},
-    guests_may_see:      %{id: "7W1DE1YAVA11AB1ET0SEENREAD", name: "Publicly visible"},
-    locals_may_interact: %{id: "710CA1SMY1NTERACTN0TREP1YY", name: "Local users may read, interact"},
-    locals_may_reply:    %{id: "710CA1SMY1NTERACTANDREP1YY", name: "Local users may read, interact and reply"},
+    guests_may_read:       %{id: "7W1DE1YAVA11AB1ET0SEENREAD", name: "Publically visible"},
+    locals_may_read:       %{id: "10CA1SMAYSEEANDREAD0N1YN0W", name: "Locally visible"},
+    locals_may_interact:   %{id: "710CA1SMY1NTERACTN0TREP1YY", name: "Local users may read and interact"},
+    locals_may_reply:      %{id: "710CA1SMY1NTERACTANDREP1YY", name: "Local users may read, interact and reply"},
     ### stereotypes - access levels
-    i_may_see:           %{id: "71MAYSEEANDREADMY0WNSTVFFS"},
-    i_may_interact:      %{id: "71MAY1NTERACTW1MY0WNSTVFFS"},
-    i_may_administer:    %{id: "71MAYADM1N1STERMY0WNSTVFFS"},
+    # i_may_* - mix one of these in as appropriate when creating something
+    i_may_read:            %{id: "71MAYSEEANDREADMY0WNSTVFFS", name: "I may read"},
+    i_may_interact:        %{id: "71MAY1NTERACTW1MY0WNSTVFFS", name: "I may read and interact"},
+    i_may_administer:      %{id: "71MAYADM1N1STERMY0WNSTVFFS", name: "I may administer"},
+    # mentions_may_* - mix one of these in when composing a custom acl for mentions
+    mentions_may_read:     %{id: "7MENT10NSCANREADTH1STH1NGS", name: "Mentions may read"},
+    mentions_may_interact: %{id: "7MENT10NSCAN1NTERACTW1TH1T", name: "Mentions may read and interact"},
+    mentions_may_reply:    %{id: "7MENT10NSCANEVENREP1YT01TS", name: "Mentions may read, interact and reply"},
     ### stereotypes - always mix in
-    negative:             %{id: "7AC0MPVTERBESAY1NGN0THANKS"},
+    negative:              %{id: "7AC0MPVTERBESAY1NGN0THANKS", name: "Negative"},
   },
   grants: %{
-    guests_may_see:      %{guest: [:read, :see]},
+    guests_may_read:     %{guest: [:read, :see]},
     locals_may_interact: %{local: [:read, :see, :mention, :tag, :boost, :flag, :like, :follow]},
     locals_may_reply:    %{local: [:read, :see, :mention, :tag, :boost, :flag, :like, :follow, :reply]},
   }
-
-
-# ladder: replyable > interactable > visible
-# circles: guests, locals, followers
-
-# common cases:
-# * public and replyable by any member or ap user
-# * public and interactable, but only replyable by people on my instance
-# * public read only, interactions and replies from people on my instance
-# * public read only, interactions and replies from people on my instance and my followers
-# * friends only:
 
 alias Bonfire.Me.Users
 
@@ -72,22 +72,20 @@ config :bonfire_me, Users,
       silence:   %{stereotype: :silence},
     },
     acls: %{
-      i_may_see:        %{stereotype: :i_may_see},
-      i_may_interact:   %{stereotype: :i_may_interact},
-      i_may_administer: %{stereotype: :i_may_administer},
-      negative:         %{stereotype: :negative, name: "Blocked"},
+      i_may_read:       %{stereotype: :i_may_read,       name: "I may read"},
+      i_may_reply:      %{stereotype: :i_may_interact,   name: "I may read, interact and reply"},
+      i_may_administer: %{stereotype: :i_may_administer, name: "I may administer"},
+      negative:         %{stereotype: :negative,         name: "Blocked"},
     },
     grants: %{
-      i_may_see:        %{SELF:  [:read, :see]},
-      i_may_interact:   %{SELF:  [:read, :see, :create, :mention, :tag, :boost, :flag, :like, :follow]},
+      i_may_read:       %{SELF:  [:read, :see]},
+      i_may_reply:      %{SELF:  [:read, :see, :create, :mention, :tag, :boost, :flag, :like, :follow, :reply]},
       i_may_administer: %{SELF:  [:read, :see, :edit, :delete]},
       negative:         %{block: block_verbs, silence: silence_verbs},
     },
     controlleds: %{
       # by default, we may administer ourselves. within contexts, we
       # may add more depending on whether the user is local or remote.
-      SELF: [:guests_may_see, :locals_may_interact, :i_may_administer]
+      SELF: [:guests_may_read, :locals_may_interact, :i_may_administer]
     },
-    # these are acls that we should always mix in. which we don't yet.
-    always: [:negative],
   }
