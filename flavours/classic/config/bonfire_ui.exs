c2a6ed895eb8960b6f7a7e6c6caa6bbd9ae6d4eb
bernini <bernini@inventati.org>
2022-03-02 18:31:41 +0100

    updated bonfire_ui

--- flavours/classic/config/bonfire_ui.exs
+++ flavours/classic/config/bonfire_ui.exs
@@ -6,7 +6,16 @@ config :bonfire, :ui,
       instance_name: "Bonfire",
       instance_logo: "https://bonfirenetworks.org/img/bonfire.png",
       instance_image: "https://bonfirenetworks.org/img/4.png",
-      instance_description: "This is a bonfire demo instance for testing purpose"
+      instance_description: "This is a bonfire demo instance for testing purpose",
+      instance_welcome_title: "👋 Welcome to our first testing instance",
+      instance_welcome_description: "Bonfire is a federated social networking toolkit for communities and individuals to design, operate and control their digital lives, by assembling their own social networks like lego blocks in order to cultivate safe and private spaces while being interconnected with the rest of the 'fediverse' and the internet at wide on their own terms. 
+The bonfire ecosystem will include:
+   1. Bonfire apps/flavours: Open source federated networks that are ready to be installed and used for different purposes. Made up of a set of pre-configured extensions.
+   2. Bonfire extensions: Forkable/customisable modules providing different features or UX, ready to be used within bonfire apps. 
+   3. Bonfire device: A plug-and-play device to have anything you need in your hands (literally).
+   4. Bonfire cloud services: Your public identity in the cloud can receive messages even when your device is offline. Syncs your Bonfire device with the fediverse, and deletes already-synced data from the cloud. Open source so others can host equivalent services.    
+
+More details at https://bonfirenetworks.org"
    ],
    sidebar_components: [
       {Bonfire.UI.Social.SidebarNavigationLive, []},
