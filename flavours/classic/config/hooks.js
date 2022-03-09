45727b0f1b262065f8e491254a5516c13fa5a011
ivanminutillo <ivanminutillo@outlook.it>
2022-03-09 10:38:57 +0100

    global typography

--- flavours/classic/config/hooks.js
+++ flavours/classic/config/hooks.js
@@ -3,7 +3,7 @@ let ExtensionHooks = {};
 // TODO: make this more configurable? ie. don't import disabled extensions
 
 // import { MyHooks } from "../../../deps/some_extension/assets/js/extension"
-import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
+import { EditorCkHooks } from "./../../../forks/bonfire_editor_ck/assets/js/extension"
 import { NotificationsHooks } from "./../../../assets/js/notifications"
 
 Object.assign(ExtensionHooks, EditorCkHooks, NotificationsHooks)
