ab019dbaa7d8f91a8db3cf09a125e47f04b8f602
Mayel de Borniol <git@mayel.space>
2022-03-10 08:52:27 +1300

    add js assets

--- flavours/classic/config/deps.js.sh
+++ flavours/classic/config/deps.js.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # any extensions/deps with a package.json in their /assets directory
-DEPS=''
+# space seperated
+DEPS='bonfire_editor_ck'
 
 chmod +x ./priv/deps.js.sh
 ./priv/deps.js.sh "$DEPS"
