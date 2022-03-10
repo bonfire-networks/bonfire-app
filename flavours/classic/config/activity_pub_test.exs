091a42f0961a47b1fa3cdd7d2c032240be94e9ec
Mayel de Borniol <git@mayel.space>
2022-03-10 13:01:20 +1300

    AP conf

--- flavours/classic/config/activity_pub_test.exs
+++ flavours/classic/config/activity_pub_test.exs
@@ -1,21 +1,10 @@
 import Config
 
-config :activity_pub, ActivityPub.TestRepo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
-  database: "bonfire_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: System.get_env("POSTGRES_HOST") || "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 60
-
-config :activity_pub, ActivityPubWeb.Endpoint,
-  http: [port: 4002],
-  server: false
+config :activity_pub, :test_repo, Bonfire.Repo
+config :activity_pub, :repo, Bonfire.Repo
+config :activity_pub, :endpoint_module, Bonfire.Web.Endpoint
 
 config :activity_pub, :adapter, Bonfire.Federate.ActivityPub.Adapter
-
-config :activity_pub, :repo, Bonfire.Repo
 
 config :activity_pub, ecto_repos: [Bonfire.Repo]
 
@@ -23,9 +12,15 @@ config :activity_pub, Oban,
   repo: Bonfire.Repo,
   queues: false
 
-config :activity_pub, :instance, federating: false
+config :activity_pub, :instance,
+  federating: false
+  # rewrite_policy: [ActivityPub.MRF.SimplePolicy]
 
 config :tesla, adapter: Tesla.Mock
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+config :activity_pub, ActivityPubWeb.Endpoint,
+  http: [port: 4002],
+  server: false
