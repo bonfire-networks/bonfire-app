d219f18242615feb1119027a4e627c2e714ba98f
Mayel de Borniol <git@mayel.space>
2022-03-13 20:02:01 +1300

    Add imagemagick

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,5 +3,9 @@ import "phoenix_html"
 
 // lightweight JS toolkit
 import Alpine from "alpinejs"
+import intersect from '@alpinejs/intersect'
+
+Alpine.plugin(intersect)
+
 window.Alpine = Alpine
 Alpine.start()
