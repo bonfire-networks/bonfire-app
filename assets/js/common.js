253ac113c21dbb3e3857c4d9d6aa5c1af3e67824
ivanminutillo <ivanminutillo@outlook.it>
2022-03-17 08:30:07 +0100

    added alpine x-compose

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,8 +4,10 @@ import "phoenix_html"
 // lightweight JS toolkit
 import Alpine from "alpinejs"
 import intersect from '@alpinejs/intersect'
+import collapse from '@alpinejs/collapse'
 
 Alpine.plugin(intersect)
+Alpine.plugin(collapse)
 
 window.Alpine = Alpine
 Alpine.start()
