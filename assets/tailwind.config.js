fe2c0cbcd12656895945e213fc4d891f7f33124d
ivanminutillo <ivanminutillo@outlook.it>
2022-03-17 09:54:53 +0100

    global

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['IBM Plex Sans', 'system-ui']
+      },
       screens: {
         'tablet': '920px',
         'tablet-lg': '1200px'
@@ -29,16 +32,6 @@ module.exports = {
         teal: colors.teal,
         cyan: colors.cyan
       },
-      ringColor: {
-        blueGray: colors.slate,
-        pink: colors.pink,
-      },
-      borderColor: {
-        blueGray: colors.slate,
-      },
-      boxShadow: {
-        tick: '0px 1px 2px rgba(0,0,0,2)'
-      },
       spacing: {
         '72': '18rem',
         '84': '21rem',
@@ -48,6 +41,7 @@ module.exports = {
       typography: (theme) => ({
         sm: {
           css: {
+            fontSize: '15px',
             h1: {
               margin: 0
             },
@@ -85,68 +79,7 @@ module.exports = {
               lineHeight: '20px'
             }
           }
-        },
-        light: {
-          css: [
-            {
-              color: theme('colors.gray.100'),
-              '[class~="lead"]': {
-                color: theme('colors.gray.100'),
-              },
-              a: {
-                color: theme('colors.white'),
-              },
-              strong: {
-                color: theme('colors.white'),
-              },
-              'ol > li::before': {
-                color: theme('colors.gray.400'),
-              },
-              'ul > li::before': {
-                backgroundColor: theme('colors.gray.600'),
-              },
-              hr: {
-                borderColor: theme('colors.gray.200'),
-              },
-              blockquote: {
-                color: theme('colors.gray.200'),
-                borderLeftColor: theme('colors.gray.600'),
-              },
-              h1: {
-                color: theme('colors.white'),
-              },
-              h2: {
-                color: theme('colors.white'),
-              },
-              h3: {
-                color: theme('colors.white'),
-              },
-              h4: {
-                color: theme('colors.white'),
-              },
-              'figure figcaption': {
-                color: theme('colors.gray.400'),
-              },
-              code: {
-                color: theme('colors.white'),
-              },
-              'a code': {
-                color: theme('colors.white'),
-              },
-              pre: {
-                color: theme('colors.gray.200'),
-                backgroundColor: theme('colors.gray.800'),
-              },
-              thead: {
-                color: theme('colors.white'),
-                borderBottomColor: theme('colors.gray.400'),
-              },
-              'tbody tr': {
-                borderBottomColor: theme('colors.gray.600'),
-              },
-            },
-          ],
-        },
+        }
       }),
     }
   },
