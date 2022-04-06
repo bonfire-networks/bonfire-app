44ab23456a25f27960354fe0212d31586a7cdcd4
ivan <bernini@inventati.org>
2022-04-06 17:10:42 +0200

    bonfire theme

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -83,6 +83,23 @@ module.exports = {
       }),
     }
   },
+  daisyui: {
+    themes: [
+      {
+        bonfire: {
+          "primary": "#fde047", 
+          "secondary": "#fb923c",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#1f2937",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",},
+      },
+      "light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"
+    ]
+  },
   variants: {
     extend: {
      ringWidth:['hover'],
