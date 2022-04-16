baa1f1f7749c84887b8b0b7053672c0d84af69c4
Mayel de Borniol <git@mayel.space>
2022-04-17 10:45:52 +1200

    fix down mig?

--- flavours/classic/repo/migrations/20220408094940_settings.exs
+++ flavours/classic/repo/migrations/20220408094940_settings.exs
@@ -2,7 +2,11 @@ defmodule Bonfire.Repo.Migrations.ImportSettings do
   use Ecto.Migration
   require Bonfire.Data.Identity.Settings.Migration
 
-  def change do
-    Bonfire.Data.Identity.Settings.Migration.migrate_settings()
+  def up do
+    Bonfire.Data.Identity.Settings.Migration.migrate_settings(:up)
+  end
+
+  def down do
+    Bonfire.Data.Identity.Settings.Migration.migrate_settings(:up)
   end
 end
