ce3562029aa66882ac6eefaa485a2950a3ce1197
ivanminutillo <ivanminutillo@outlook.it>
2022-04-28 18:27:27 +0200

    global and added quill to deps

--- flavours/classic/config/deps_hooks.js
+++ flavours/classic/config/deps_hooks.js
@@ -9,8 +9,10 @@ import { InputSelectHooks } from "./../../../deps/bonfire_common/assets/js/input
 import { NotificationsHooks } from "./../../../deps/bonfire_common/assets/js/notifications"
 import { ThemeHooks } from "./../../../deps/bonfire_ui_social/assets/js/theme"
 import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
+import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js/extension"
+
 // import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 
-Object.assign(ExtensionHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, EditorCkHooks)
+Object.assign(ExtensionHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, EditorCkHooks, EditorQuillHooks)
  
 export { ExtensionHooks }
