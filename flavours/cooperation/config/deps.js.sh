288b4173c9219dd63486c27e1fee78d009053310
Mayel de Borniol <git@mayel.space>
2022-05-02 19:57:35 +1200

    Fixes

--- flavours/cooperation/config/deps.js.sh
+++ flavours/cooperation/config/deps.js.sh
@@ -4,7 +4,7 @@
 # NOTE: any LV Hooks should also be added to ./deps_hooks.js
 // TODO: make this more configurable? ie. autogenerate from active extensions with JS assets
 
-DEPS='bonfire_common bonfire_ui_social bonfire_editor_ck bonfire_geolocate bonfire_ui_kanban'
+DEPS='bonfire_common bonfire_ui_social bonfire_editor_ck bonfire_editor_quill bonfire_geolocate bonfire_ui_kanban'
 
 chmod +x ./priv/deps.js.sh
 ./priv/deps.js.sh "$DEPS"
