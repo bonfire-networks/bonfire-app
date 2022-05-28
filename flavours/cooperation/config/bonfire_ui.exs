41567404349a83952e426028a757ab6a1f9e279f
Mayel de Borniol <git@mayel.space>
2022-05-29 00:14:49 +1200

    https://github.com/bonfire-networks/bonfire-app/issues/319

--- flavours/cooperation/config/bonfire_ui.exs
+++ flavours/cooperation/config/bonfire_ui.exs
@@ -33,6 +33,7 @@ More details at https://bonfirenetworks.org",
       # {Bonfire.UI.ValueFlows.ProcessesListLive, [title: "Processes", process_url: "/process/"]},
       # {Bonfire.UI.ValueFlows.ProcessesListLive, [title: "Lists", process_url: "/breadpub/list/"]}
    ],
+   rich_text_editor: Bonfire.Editor.Quill,
    smart_input: [
       post: true,
       cw: true,
