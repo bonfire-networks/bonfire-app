12b926bbeb07a2172d64d1c6804b5f96236eec1a
Alejandro Baez <develop@a.baez.link>
2022-07-03 12:51:31 -0500

    feat: add missing props.nix

---
+++ props.nix
@@ -0,0 +1,10 @@
+{
+  # the name of the package
+  app_name = "bonfire";
+  # the version of the package
+  app_version = "0.3.0-classic-beta.7";
+  # version release of elixir
+  elixir_release = "1.13";
+  # version release of erlang
+  erlang_release = "24";
+}
