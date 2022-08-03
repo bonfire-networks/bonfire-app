ec809843ebaf54f3220d35707be4757bcdf283eb
ivanminutillo <ivanminutillo@outlook.it>
2022-08-03 15:29:26 +0200

    tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       screens: {
         'tablet': '920px',
         'tablet-lg': '1200px',
+        'desktop-lg': '1464px',
         'wide': '1920px'
       },
       maxWidth: {
