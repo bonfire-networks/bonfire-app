2910c4add00dd6e6841e17eab3ed993ac6797d65
Mayel de Borniol <git@mayel.space>
2022-08-07 19:46:32 +1200

    fetch iconify_ex JS deps

--- flavours/classic/config/deps.js.sh
+++ flavours/classic/config/deps.js.sh
@@ -2,7 +2,7 @@
 
 # any extensions/deps with a package.json in their /assets directory
 # space seperated
-DEPS='bonfire_ui_common bonfire_editor_ck bonfire_editor_quill'
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_ck bonfire_editor_quill'
 
 chmod +x ./assets/install_extensions.sh
 ./assets/install_extensions.sh "$DEPS" $@
--- flavours/cooperation/config/deps.js.sh
+++ flavours/cooperation/config/deps.js.sh
@@ -4,7 +4,7 @@
 # NOTE: any LV Hooks should also be added to ./deps_hooks.js
 # TODO: make this more configurable? ie. autogenerate from active extensions with JS assets
 
-DEPS='bonfire_ui_common bonfire_editor_ck bonfire_editor_quill bonfire_geolocate bonfire_ui_kanban'
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_ck bonfire_editor_quill bonfire_geolocate bonfire_ui_kanban'
 
 chmod +x ./assets/install_extensions.sh
 ./assets/install_extensions.sh "$DEPS" $@
