811adbbe3f921939946acedad842e327bf6473df
Mayel de Borniol <git@mayel.space>
2022-08-07 19:46:32 +1200

    fetch iconify_ex JS deps

--- flavours/haha/config/deps.js.sh
+++ flavours/haha/config/deps.js.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # any extensions/deps with a package.json in their /assets directory
-DEPS=''
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_ck bonfire_editor_quill'
 
 chmod +x ./assets/install_extensions.sh
 ./assets/install_extensions.sh "$DEPS" $@
--- flavours/upcycle/config/deps.js.sh
+++ flavours/upcycle/config/deps.js.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # any extensions/deps with a package.json in their /assets directory
-DEPS='bonfire_ui_common bonfire_editor_ck bonfire_geolocate'
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_ck bonfire_geolocate'
 
 chmod +x ./assets/install_extensions.sh
 ./assets/install_extensions.sh "$DEPS" $@
