3540ff48a25d75de760fe453e801d8b6b06e219d
Mayel de Borniol <git@mayel.space>
2022-08-08 10:39:50 +1200

    More icons

---
+++ lib/web/icons/bx/block.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Bx.Block do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10s10-4.486 10-10S17.514 2 12 2zM4 12c0-1.846.634-3.542 1.688-4.897l11.209 11.209A7.946 7.946 0 0 1 12 20c-4.411 0-8-3.589-8-8zm14.312 4.897L7.103 5.688A7.948 7.948 0 0 1 12 4c4.411 0 8 3.589 8 8a7.954 7.954 0 0 1-1.688 4.897z"/></svg>
+    """
+  end
+end
---
+++ lib/web/icons/bxs/user-badge.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Bxs.UserBadge do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" d="M17.988 22a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h11.988zM9 5h6v2H9V5zm5.25 6.25A2.26 2.26 0 0 1 12 13.501c-1.235 0-2.25-1.015-2.25-2.251S10.765 9 12 9a2.259 2.259 0 0 1 2.25 2.25zM7.5 18.188c0-1.664 2.028-3.375 4.5-3.375s4.5 1.711 4.5 3.375v.563h-9v-.563z"/></svg>
+    """
+  end
+end
