881d11c09166dfe53859223da3bde12df3458a05
ivanminutillo <ivanminutillo@outlook.it>
2022-08-09 09:28:37 +0200

    icons

--- lib/web/icons/heroicons-solid/login.ex
+++ lib/web/icons/heroicons-solid/login.ex
@@ -2,7 +2,11 @@ defmodule Iconify.HeroiconsSolid.Login do
   use Phoenix.Component
   def render(assigns) do
     ~H"""
+<<<<<<< HEAD
     <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M3 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1Zm7.707 3.293a1 1 0 0 1 0 1.414L9.414 9H17a1 1 0 1 1 0 2H9.414l1.293 1.293a1 1 0 0 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 0Z" clip-rule="evenodd"/></svg>
+=======
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M3 3a1 1 0 0 1 1 1v12a1 1 0 1 1-2 0V4a1 1 0 0 1 1-1Zm7.707 3.293a1 1 0 0 1 0 1.414L9.414 9H17a1 1 0 1 1 0 2H9.414l1.293 1.293a1 1 0 0 1-1.414 1.414l-3-3a1 1 0 0 1 0-1.414l3-3a1 1 0 0 1 1.414 0Z" clip-rule="evenodd"/></svg>
+>>>>>>> df3f3d636f (changelog)
     """
   end
 end
