7b8eddedd0bb073b833652f8631332861470c18b
ivan <ivanminutillo@outlook.it>
2022-08-21 09:49:16 +0000

    nav style

---
+++ lib/web/icons/heroicons-solid/exclamation.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsSolid.Exclamation do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92ZM11 13a1 1 0 1 1-2 0a1 1 0 0 1 2 0Zm-1-8a1 1 0 0 0-1 1v3a1 1 0 0 0 2 0V6a1 1 0 0 0-1-1Z" clip-rule="evenodd"/></svg>
+    """
+  end
+end
