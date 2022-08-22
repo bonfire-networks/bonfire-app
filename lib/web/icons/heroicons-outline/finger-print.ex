3df6f6e6bddb192178b444e47102a9e66ab85cea
ivan <ivanminutillo@outlook.it>
2022-08-22 17:36:47 +0000

    up

---
+++ lib/web/icons/heroicons-outline/finger-print.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.FingerPrint do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 0 0 8 11a4 4 0 1 1 8 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0 0 15.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 0 0 8 4.07M3 15.364c.64-1.319 1-2.8 1-4.364c0-1.457.39-2.823 1.07-4"/></svg>
+    """
+  end
+end
