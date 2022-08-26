a6e9b5cc1c9d43e3f536c20909a99e379dd5603d
ivan <ivanminutillo@outlook.it>
2022-08-26 11:42:07 +0000

    icons

---
+++ lib/web/icons/heroicons-outline/globe.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.Globe do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 0 1 2 2v1a2 2 0 0 0 2 2a2 2 0 0 1 2 2v2.945M8 3.935V5.5A2.5 2.5 0 0 0 10.5 8h.5a2 2 0 0 1 2 2a2 2 0 1 0 4 0a2 2 0 0 1 2-2h1.064M15 20.488V18a2 2 0 0 1 2-2h3.064M21 12a9 9 0 1 1-18 0a9 9 0 0 1 18 0Z"/></svg>
+    """
+  end
+end
