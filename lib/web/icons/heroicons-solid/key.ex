018cab01c4fa3bdcc9f75d30bd6012e340d1566e
ivanminutillo <ivanminutillo@outlook.it>
2022-09-06 23:05:54 +0200

    up

---
+++ lib/web/icons/heroicons-solid/key.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsSolid.Key do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M18 8a6 6 0 0 1-7.743 5.743L10 14l-1 1l-1 1H6v2H2v-4l4.257-4.257A6 6 0 1 1 18 8Zm-6-4a1 1 0 1 0 0 2a2 2 0 0 1 2 2a1 1 0 1 0 2 0a4 4 0 0 0-4-4Z" clip-rule="evenodd"/></svg>
+    """
+  end
+end
---
+++ lib/web/icons/heroicons-solid/minus-circle.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsSolid.MinusCircle do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16a8 8 0 0 0 0 16ZM7 9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2H7Z" clip-rule="evenodd"/></svg>
+    """
+  end
+end
