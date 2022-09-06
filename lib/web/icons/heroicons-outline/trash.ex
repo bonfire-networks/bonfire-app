6aac22349c5c9d70370f6287d2e4214d99164c69
Mayel de Borniol <git@mayel.space>
2022-09-07 08:59:28 +1200

    misc

---
+++ lib/web/icons/heroicons-outline/trash.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.Trash do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 7l-.867 12.142A2 2 0 0 1 16.138 21H7.862a2 2 0 0 1-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v3M4 7h16"/></svg>
+    """
+  end
+end
