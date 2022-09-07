7c4e0b022cd2434fa0a218346f0e06c58c1a7c49
ivanminutillo <ivanminutillo@outlook.it>
2022-09-07 20:42:03 +0200

    style

---
+++ lib/web/icons/heroicons-outline/newspaper.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.Newspaper do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v1m2 13a2 2 0 0 1-2-2V7m2 13a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8Z"/></svg>
+    """
+  end
+end
---
+++ lib/web/icons/heroicons-solid/inbox.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsSolid.Inbox do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M5 3a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5Zm0 2h10v7h-2l-1 2H8l-1-2H5V5Z" clip-rule="evenodd"/></svg>
+    """
+  end
+end
