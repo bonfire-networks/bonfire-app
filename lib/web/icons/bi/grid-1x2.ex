8db7282a63f6090a08993fed88a4010e9a84cd28
ivanminutillo <ivanminutillo@outlook.it>
2022-09-16 11:11:09 +0200

    responsive

---
+++ lib/web/icons/bi/grid-1x2.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Bi.Grid1x2 do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 16 16" aria-hidden="true"><path fill="currentColor" d="M6 1H1v14h5V1zm9 0h-5v5h5V1zm0 9v5h-5v-5h5zM0 1a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm9 0a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V1zm1 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1h-5z"/></svg>
+    """
+  end
+end
