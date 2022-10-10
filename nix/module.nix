c348464ad72e528efb2b83d6430107f38c28c721
Anish Lakhwara <anish+git@lakhwara.com>
2022-10-10 22:13:59 +1000

    clean up some more

--- nix/module.nix
+++ nix/module.nix
@@ -1,19 +1,23 @@
 { pkgs, config, lib, ... }:
 with lib;
 let
-  bonfireConfig = config.services.bonfire;
+  cfg = config.services.bonfire;
 in
 {
   options.services.bonfire = {
+    enable = mkEnableOption "Enable Bonfire";
+
     port = mkOption {
       type = types.port;
       default = 4000;
       description = "port to run the instance backend on";
     };
+
     package = mkOption {
       type = types.package;
       description = "package to run the instance with";
     };
+
     hostname = mkOption {
       type = types.str;
       default = "bonfire.cafe";
@@ -22,6 +26,7 @@ in
         hostname for which the service will be run
       '';
     };
+
     dbName = mkOption {
       type = types.str;
       default = "bonfire";
@@ -29,6 +34,7 @@ in
         name of the database you want to connect to
       '';
     };
+
     dbSocketDir = mkOption {
       type = types.nullOr types.str;
       default = null;
@@ -37,6 +43,26 @@ in
         with a unix socket and not TCP/IP
       '';
     };
+
+    user = mkOption {
+      type = types.str;
+      default = "bonfire";
+      description = "User account under which pleroma runs.";
+    };
+
+    group = mkOption {
+      type = types.str;
+      default = "bonfire";
+      description = "Group account under which pleroma runs.";
+    };
+
+    stateDir = mkOption {
+      type = types.str;
+      default = "/var/lib/bonfire";
+      readOnly = true;
+      description = "Directory where the pleroma service will save the uploads and static files.";
+    };
+
     environmentFile = mkOption {
       type = types.nullOr types.path;
       default = null;
@@ -51,16 +77,29 @@ in
     };
   };
 
-  config = lib.mkIf config.bonfireConfig.enable {
+  config = lib.mkIf cfg.enable {
     services.postgresql = {
       extraPlugins = with pkgs.postgresql_13.pkgs; [ postgis ];
-      ensureDatabases = [ dbName ];
+      ensureDatabases = [ cfg.dbName ];
       ensureUsers = [{
         # Same name as the unix user is needed
-        name = "bonfire";
-        ensurePermissions = { "DATABASE ${dbName}" = "ALL PRIVILEGES"; };
+        name = cfg.user;
+        ensurePermissions = { "DATABASE ${cfg.dbName}" = "ALL PRIVILEGES"; };
       }];
     };
+
+    users = {
+      users."${cfg.user}" = {
+        description = "Bonfire user";
+        home = cfg.stateDir;
+        group = cfg.group;
+        createHome = true;
+        isSystemUser = true;
+      };
+      groups."${cfg.group}" = { };
+    };
+
+    environment.systemPackages = [ cfg.package ];
 
     systemd.services.bonfire = {
       wantedBy = [ "multi-user.target" ];
@@ -69,35 +108,40 @@ in
       description = "Bonfire!";
       serviceConfig = {
         Type = "exec";
+        User = cfg.user;
+        Group = cfg.group;
+        DynamicUser = true;
         Restart = "on-failure";
         RestartSec = 5;
-        ExecStartPre = "${package}/bin/bonfire eval Bonfire.Repo.ReleaseTasks.migrate";
-        ExecStart = "${package}/bin/bonfire start";
-        ExecStop = "${package}/bin/bonfire stop";
+        ExecStartPre = "${cfg.package}/bin/bonfire eval Bonfire.Repo.ReleaseTasks.migrate";
+        ExecStart = "${cfg.package}/bin/bonfire start";
+        ExecStop = "${cfg.package}/bin/bonfire stop";
 
-        DynamicUser = true;
         StateDirectory = "bonfire";
+        WorkingDirectory = cfg.stateDir;
+        StateDirectoryMode = "700";
 
-        EnvironmentFile = environmentFile;
+        EnvironmentFile = cfg.environmentFile;
 
         PrivateTmp = true;
+        PrivateHome = true;
         ProtectSystem = "full";
         NoNewPrivileges = true;
 
-        ReadWritePaths = "${if dbSocketDir == null then "" else dbSocketDir} /var/lib/bonfire";
+        ReadWritePaths = "${if cfg.dbSocketDir == null then "" else cfg.dbSocketDir} ${cfg.stateDir}";
       };
       environment = {
         RELEASE_TMP = "/tmp";
-        TZDATA_DIR = "/var/lib/bonfire";
+        TZDATA_DIR = cfg.stateDir;
         LANG = "en_US.UTF-8";
-        PORT = toString port;
-        POSTGRES_USER = "bonfire";
-        POSTGRES_DB = dbName;
-        POSTGRES_SOCKET_DIR = lib.mkIf (dbSocketDir != null) dbSocketDir;
-        HOSTNAME = hostname;
+        PORT = toString cfg.port;
+        POSTGRES_USER = cfg.user;
+        POSTGRES_DB = cfg.dbName;
+        POSTGRES_SOCKET_DIR = lib.mkIf (cfg.dbSocketDir != null) cfg.dbSocketDir;
+        HOSTNAME = cfg.hostname;
         WITH_DOCKER = "no";
-        FLAVOUR = "cooperation";
-        BONFIRE_FLAVOUR = "flavours/cooperation";
+        FLAVOUR = "classic";
+        BONFIRE_FLAVOUR = "flavours/classic";
       };
     };
   };
