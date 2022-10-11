7a5a3a43d244c1cb34d7f935211059fbb238aa87
Mayel de Borniol <git@mayel.space>
2022-10-12 12:15:40 +1300

    Icons

--- lib/web/icons/heroicons-outline/paper-airplane.ex
+++ lib/web/icons/heroicons-outline/paper-airplane.ex
@@ -1,8 +1,25 @@
 defmodule Iconify.HeroiconsOutline.PaperAirplane do
   use Phoenix.Component
+
   def render(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m12 19l9 2l-9-18l-9 18l9-2Zm0 0v-8"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="m12 19l9 2l-9-18l-9 18l9-2Zm0 0v-8"
+      />
+    </svg>
     """
   end
 end
