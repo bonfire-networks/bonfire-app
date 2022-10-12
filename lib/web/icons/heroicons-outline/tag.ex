97a95dbd293f81069a0f52d6985392f6cade3183
ivanminutillo <ivanminutillo@outlook.it>
2022-10-12 10:47:30 +0200

    up

---
+++ lib/web/icons/heroicons-outline/tag.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.HeroiconsOutline.Tag do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5a1.99 1.99 0 0 1 1.414.586l7 7a2 2 0 0 1 0 2.828l-7 7a2 2 0 0 1-2.828 0l-7-7A1.994 1.994 0 0 1 3 12V7a4 4 0 0 1 4-4Z"/></svg>
+    """
+  end
+end
