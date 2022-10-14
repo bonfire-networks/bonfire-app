a96a05a5b32a90d5cc543ff4cc4bec29d35b1966
Mayel de Borniol <git@mayel.space>
2022-10-14 22:23:11 +1300

    Add pinning

---
+++ flavours/cooperation/repo/migrations/20221014084501_pin.exs
@@ -0,0 +1,12 @@
+defmodule Bonfire.Repo.Migrations.Pin do
+  use Ecto.Migration
+  require Bonfire.Data.Social.Pin.Migration
+
+  def up do
+    Bonfire.Data.Social.Pin.Migration.migrate_pin()
+  end
+
+  def down do
+    Bonfire.Data.Social.Pin.Migration.migrate_pin()
+  end
+end
