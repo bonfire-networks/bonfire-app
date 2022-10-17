b08fdf0ab21ab5fb5b8733758328ea36d3df98ba
Mayel de Borniol <git@mayel.space>
2022-10-17 14:02:31 +1300

    misc

--- benchmarks/feeds.ex
+++ benchmarks/feeds.ex
@@ -1,11 +1,15 @@
+# NOTE: make sure you populate your local with seeds first, and then copy paste this in iex
+Logger.configure(level: :warn)
 Benchee.run(
   %{
-    "feed with boundaries & minimal join/preloads" => fn -> Bonfire.Social.FeedActivities.feed(:local, preloads: :with_object) end,
-    "feed without boundaries & minimal join/preloads" => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true, preloads: :with_object) end,
-    "feed with boundaries & cached preloads" => fn -> Bonfire.Social.FeedActivities.feed(:local) |> Bonfire.Social.Feeds.LiveHandler.preloads() end,
-    "feed without boundaries & cached preloads" => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true) |> Bonfire.Social.Feeds.LiveHandler.preloads() end,
-    "feed with boundaries & full join/preloads" => fn -> Bonfire.Social.FeedActivities.feed(:local, preloads: :feed) end,
-    "feed without boundaries & with full join/preloads " => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true, preloads: :feed) end,
+    "minimal join/preloads, with boundaries applied" => fn -> Bonfire.Social.FeedActivities.feed(:local, preloads: :with_object) end,
+    "minimal join/preloads, without boundaries applied" => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true, preloads: :with_object) end,
+
+    "caching preloads, with boundaries applied" => fn -> Bonfire.Social.FeedActivities.feed(:local) |> Bonfire.Social.Feeds.LiveHandler.preloads(with_cache: true) end,
+    "caching preloads, without boundaries applied " => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true) |> Bonfire.Social.Feeds.LiveHandler.preloads(with_cache: true) end,
+
+    "full join/preloads, with boundaries applied" => fn -> Bonfire.Social.FeedActivities.feed(:local, preloads: :feed) end,
+    "full join/preloads, without boundaries applied" => fn -> Bonfire.Social.FeedActivities.feed(:local, skip_boundary_check: true, preloads: :feed) end,
     # "AP:shared_outbox" => fn -> ActivityPubWeb.ObjectView.render("outbox.json", %{outbox: :shared_outbox}) end
   },
   parallel: 10,
@@ -19,3 +23,4 @@ Benchee.run(
     Benchee.Formatters.Console
   ]
 )
+Logger.configure(level: :debug)
