00017c697ba00f1bfd720136e9b0aba3228f9567
ivanminutillo <ivanminutillo@outlook.it>
2022-10-26 17:06:02 +0200

    up

---
+++ lib/web/icons/heroicons-solid/filter.ex
@@ -0,0 +1,23 @@
+defmodule Iconify.HeroiconsSolid.Filter do
+  use Phoenix.Component
+
+  def render(assigns) do
+    ~H"""
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M3 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-.293.707L12 11.414V15a1 1 0 0 1-.293.707l-2 2A1 1 0 0 1 8 17v-5.586L3.293 6.707A1 1 0 0 1 3 6V3Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    """
+  end
+end
