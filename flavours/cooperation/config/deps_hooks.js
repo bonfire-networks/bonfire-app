20db47e08b3870a7c98c1ec9acf17aa74ba7e36f
ivanminutillo <ivanminutillo@outlook.it>
2022-10-27 18:00:27 +0200

    up

--- flavours/cooperation/config/deps_hooks.js
+++ flavours/cooperation/config/deps_hooks.js
@@ -7,7 +7,7 @@ let ExtensionHooks = {};
 import { ChangeLocaleHooks } from "./../../../deps/bonfire_ui_common/assets/js/change_locale"
 import { InputSelectHooks } from "./../../../deps/bonfire_ui_common/assets/js/input_select"
 import { NotificationsHooks } from "./../../../deps/bonfire_ui_common/assets/js/notifications"
-import { EmojiHooks } from "./../../../deps/bonfire_ui_common/assets/js/emoji"
+import { CarouselHooks } from "./../../../forks/bonfire_ui_common/assets/js/carousel"
 import { ThemeHooks } from "./../../../deps/bonfire_ui_common/assets/js/theme"
 import { FeedHooks } from "./../../../deps/bonfire_ui_common/assets/js/feed"
 import { ImageHooks } from "./../../../deps/bonfire_ui_common/assets/js/image"
@@ -17,6 +17,6 @@ import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js
 import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
 
-Object.assign(ExtensionHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks, EditorQuillHooks, GeolocateHooks, KanbanHooks) // EditorCkHooks
+Object.assign(ExtensionHooks, CarouselHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks, EditorQuillHooks, GeolocateHooks, KanbanHooks) // EditorCkHooks
  
 export { ExtensionHooks }
