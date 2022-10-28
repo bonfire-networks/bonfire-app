5a0dd4d2760c8f3a1232264679afb8b82a238af2
ivanminutillo <ivanminutillo@outlook.it>
2022-10-28 17:48:27 +0200

    up

---
+++ lib/web/icons/akar-icons/chat-dots.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.AkarIcons.ChatDots do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2"><path stroke-linejoin="round" d="M14 19c3.771 0 5.657 0 6.828-1.172C22 16.657 22 14.771 22 11c0-3.771 0-5.657-1.172-6.828C19.657 3 17.771 3 14 3h-4C6.229 3 4.343 3 3.172 4.172C2 5.343 2 7.229 2 11c0 3.771 0 5.657 1.172 6.828c.653.654 1.528.943 2.828 1.07"/><path d="M12 11v.01M8 11v.01m8-.01v.01M14 19c-1.236 0-2.598.5-3.841 1.145c-1.998 1.037-2.997 1.556-3.489 1.225c-.492-.33-.399-1.355-.212-3.404L6.5 17.5"/></g></svg>
+    """
+  end
+end
