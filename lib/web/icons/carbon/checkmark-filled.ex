75210a09e43c0738df6df14432da6da29273ee85
ivanminutillo <ivanminutillo@outlook.it>
2022-11-01 12:05:38 +0100

    up

---
+++ lib/web/icons/carbon/checkmark-filled.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Carbon.CheckmarkFilled do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 32 32" aria-hidden="true"><path fill="currentColor" d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2Zm-2 19.59l-5-5L10.59 15L14 18.41L21.41 11l1.596 1.586Z"/><path fill="none" d="m14 21.591l-5-5L10.591 15L14 18.409L21.41 11l1.595 1.585L14 21.591z"/></svg>
+    """
+  end
+end
