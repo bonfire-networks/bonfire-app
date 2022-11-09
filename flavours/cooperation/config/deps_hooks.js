3faed38b242103f8a3cfaad54067f952a641a8a5
ivanminutillo <ivanminutillo@outlook.it>
2022-11-09 17:51:13 +0100

    up

--- flavours/cooperation/config/deps_hooks.js
+++ flavours/cooperation/config/deps_hooks.js
@@ -9,7 +9,7 @@ import { InputSelectHooks } from "./../../../deps/bonfire_ui_common/assets/js/in
 import { NotificationsHooks } from "./../../../deps/bonfire_ui_common/assets/js/notifications"
 import { CarouselHooks } from "./../../../deps/bonfire_ui_common/assets/js/carousel"
 import { ThemeHooks } from "./../../../deps/bonfire_ui_common/assets/js/theme"
-import { FeedHooks } from "./../../../deps/bonfire_ui_common/assets/js/feed"
+import { FeedHooks } from "./../../../forks/bonfire_ui_common/assets/js/feed"
 import { ImageHooks } from "./../../../deps/bonfire_ui_common/assets/js/image"
 // import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
 import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js/extension"
