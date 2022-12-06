682efc82b2bb254a5509d42cb74a5ca927dca652
Mayel de Borniol <git@mayel.space>
2022-12-06 19:14:39 +1300

    JS

--- flavours/classic/config/deps_hooks.js
+++ flavours/classic/config/deps_hooks.js
@@ -7,15 +7,19 @@ let ExtensionHooks = {};
 import { ChangeLocaleHooks } from "./../../../deps/bonfire_ui_common/assets/js/change_locale"
 import { InputSelectHooks } from "./../../../deps/bonfire_ui_common/assets/js/input_select"
 import { NotificationsHooks } from "./../../../deps/bonfire_ui_common/assets/js/notifications"
-// import { EmojiHooks } from "./../../../deps/bonfire_ui_common/assets/js/emoji"
+import { CarouselHooks } from "./../../../deps/bonfire_ui_common/assets/js/carousel"
+import { ResponsiveTabsHooks } from "./../../../deps/bonfire_ui_common/assets/js/responsive_tabs"
 import { ThemeHooks } from "./../../../deps/bonfire_ui_common/assets/js/theme"
 import { FeedHooks } from "./../../../deps/bonfire_ui_common/assets/js/feed"
 import { ImageHooks } from "./../../../deps/bonfire_ui_common/assets/js/image"
 // import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
 import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js/extension"
-// import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 
-Object.assign(ExtensionHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks, EditorQuillHooks) // EditorCkHooks, GeolocateHooks
- 
+// import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
+// import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
+
+// import { EncryptHooks } from "./../../../deps/bonfire_encrypt/assets/js/extension"
+
+Object.assign(ExtensionHooks, ResponsiveTabsHooks, EditorQuillHooks, CarouselHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks) // EditorCkHooks
+
 export { ExtensionHooks }
- 
