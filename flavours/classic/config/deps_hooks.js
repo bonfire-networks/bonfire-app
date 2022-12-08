19ced6ed4904de10741df59f4c7cffabd87ff354
ivanminutillo <ivanminutillo@outlook.it>
2022-12-08 09:32:35 +0100

    up

--- flavours/classic/config/deps_hooks.js
+++ flavours/classic/config/deps_hooks.js
@@ -12,14 +12,17 @@ import { ResponsiveTabsHooks } from "./../../../deps/bonfire_ui_common/assets/js
 import { ThemeHooks } from "./../../../deps/bonfire_ui_common/assets/js/theme"
 import { FeedHooks } from "./../../../deps/bonfire_ui_common/assets/js/feed"
 import { ImageHooks } from "./../../../deps/bonfire_ui_common/assets/js/image"
-// import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
-import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js/extension"
+import { EmojiHooks } from "./../../../deps/bonfire_ui_common/assets/js/emoji"
+import { EditorCkHooks } from "./../../../deps/bonfire_editor_ck/assets/js/extension"
+import { ComposerHooks } from "./../../../deps/bonfire_ui_common/assets/js/composer"
+
+// import { EditorQuillHooks } from "./../../../deps/bonfire_editor_quill/assets/js/extension"
 
 // import { GeolocateHooks } from "./../../../deps/bonfire_geolocate/assets/js/extension"
 // import { KanbanHooks } from "./../../../deps/bonfire_ui_kanban/assets/js/extension"
 
 // import { EncryptHooks } from "./../../../deps/bonfire_encrypt/assets/js/extension"
 
-Object.assign(ExtensionHooks, ResponsiveTabsHooks, EditorQuillHooks, CarouselHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks) // EditorCkHooks
+Object.assign(ExtensionHooks, ComposerHooks, EmojiHooks, ResponsiveTabsHooks, EditorCkHooks, CarouselHooks, FeedHooks, ChangeLocaleHooks, InputSelectHooks, NotificationsHooks, ThemeHooks, ImageHooks) // EditorCkHooks
 
 export { ExtensionHooks }
