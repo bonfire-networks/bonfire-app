2b62b8e67e6f3c8ba24ba6fa17c22bec3ba3e2b5
ivanminutillo <ivanminutillo@outlook.it>
2022-12-08 09:32:35 +0100

    up

--- flavours/classic/config/bonfire_ui.exs
+++ flavours/classic/config/bonfire_ui.exs
@@ -26,8 +26,9 @@ More details at https://bonfirenetworks.org",
   # end theme
   hide_app_switcher: true,
   # rich_text_editor_disabled: true,
-  rich_text_editor: Bonfire.Editor.Quill,
+  # rich_text_editor: Bonfire.Editor.Quill,
   # rich_text_editor: Bonfire.Editor.Ck,
+  rich_text_editor: Bonfire.UI.Common.ComposerLive,
   # default
   font_family: "Inter (Latin Languages)",
   font_families: [
