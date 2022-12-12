015c5b3f0e7e839b2bb3c4525f1e1a8c2d5dc3d8
ivanminutillo <ivanminutillo@outlook.it>
2022-12-12 13:26:39 +0100

    icons

---
+++ lib/web/icons/octicon/x-16.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Octicon.X16 do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 16 16" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M3.72 3.72a.75.75 0 0 1 1.06 0L8 6.94l3.22-3.22a.75.75 0 1 1 1.06 1.06L9.06 8l3.22 3.22a.75.75 0 1 1-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 0 1-1.06-1.06L6.94 8L3.72 4.78a.75.75 0 0 1 0-1.06z"/></svg>
+    """
+  end
+end
