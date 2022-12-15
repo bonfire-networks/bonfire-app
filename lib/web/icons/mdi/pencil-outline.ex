07ca0c9f5b22becf9439ae6cb29285b8480d8da6
ivanminutillo <ivanminutillo@outlook.it>
2022-12-15 17:04:18 +0100

    up

---
+++ lib/web/icons/mdi/pencil-outline.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Mdi.PencilOutline do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="m14.06 9l.94.94L5.92 19H5v-.92L14.06 9m3.6-6c-.25 0-.51.1-.7.29l-1.83 1.83l3.75 3.75l1.83-1.83c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75Z"/></svg>
+    """
+  end
+end
