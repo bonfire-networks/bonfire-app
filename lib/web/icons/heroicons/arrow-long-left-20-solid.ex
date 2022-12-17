8088eee5305b9f7acd3cbd9854d41e4ce27773b4
Mayel de Borniol <git@mayel.space>
2022-12-18 10:17:40 +1300

    f

--- lib/web/icons/heroicons/arrow-long-left-20-solid.ex
+++ lib/web/icons/heroicons/arrow-long-left-20-solid.ex
@@ -1,8 +1,23 @@
 defmodule Iconify.Heroicons.ArrowLongLeft20Solid do
   use Phoenix.Component
+
   def render(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z" clip-rule="evenodd"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M18 10a.75.75 0 0 1-.75.75H4.66l2.1 1.95a.75.75 0 1 1-1.02 1.1l-3.5-3.25a.75.75 0 0 1 0-1.1l3.5-3.25a.75.75 0 1 1 1.02 1.1l-2.1 1.95h12.59A.75.75 0 0 1 18 10Z"
+        clip-rule="evenodd"
+      />
+    </svg>
     """
   end
 end
--- lib/web/icons/heroicons/arrow-long-right-20-solid.ex
+++ lib/web/icons/heroicons/arrow-long-right-20-solid.ex
@@ -1,8 +1,23 @@
 defmodule Iconify.Heroicons.ArrowLongRight20Solid do
   use Phoenix.Component
+
   def render(assigns) do
     ~H"""
-    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 20 20" aria-hidden="true"><path fill="currentColor" fill-rule="evenodd" d="M2 10a.75.75 0 0 1 .75-.75h12.59l-2.1-1.95a.75.75 0 1 1 1.02-1.1l3.5 3.25a.75.75 0 0 1 0 1.1l-3.5 3.25a.75.75 0 1 1-1.02-1.1l2.1-1.95H2.75A.75.75 0 0 1 2 10Z" clip-rule="evenodd"/></svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      class={@class}
+      viewBox="0 0 20 20"
+      aria-hidden="true"
+    >
+      <path
+        fill="currentColor"
+        fill-rule="evenodd"
+        d="M2 10a.75.75 0 0 1 .75-.75h12.59l-2.1-1.95a.75.75 0 1 1 1.02-1.1l3.5 3.25a.75.75 0 0 1 0 1.1l-3.5 3.25a.75.75 0 1 1-1.02-1.1l2.1-1.95H2.75A.75.75 0 0 1 2 10Z"
+        clip-rule="evenodd"
+      />
+    </svg>
     """
   end
 end
