ea8e233410ee16464decd79c3ed97edbe715b32d
ivanminutillo <ivanminutillo@outlook.it>
2022-12-22 17:55:55 +0100

    up

---
+++ lib/web/icons/mdi/arrow-left.ex
@@ -0,0 +1,8 @@
+defmodule Iconify.Mdi.ArrowLeft do
+  use Phoenix.Component
+  def render(assigns) do
+    ~H"""
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" class={@class} viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M20 11v2H8l5.5 5.5l-1.42 1.42L4.16 12l7.92-7.92L13.5 5.5L8 11h12Z"/></svg>
+    """
+  end
+end
