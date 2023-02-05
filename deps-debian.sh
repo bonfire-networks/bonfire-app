e98e8c94c784dc52c2194db483c8a695ea93c08b
Mayel de Borniol <git@mayel.space>
2023-02-06 11:55:16 +1300

    hmm

--- deps-debian.sh
+++ deps-debian.sh
@@ -1,5 +1,5 @@
 apt-get update -q -y
-apt-get install -q -y git tar file gcc sqlite3 yarnpkg mailcap ca-certificates libssl-dev tzdata gettext curl rustc cargo wget
+apt-get install -q -y git tar file gcc sqlite3 yarnpkg mailcap ca-certificates libssl-dev tzdata gettext curl rustc cargo wget gnupg
 
 bash -ci "$(wget -qO - 'https://shlink.makedeb.org/install')"
 git clone 'https://mpr.makedeb.org/just' && cd just && makedeb -si
