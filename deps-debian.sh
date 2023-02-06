db1535ec0e7806d4a3ac310e2549106d79b88b75
Mayel de Borniol <git@mayel.space>
2023-02-06 15:47:30 +1300

    erl

--- deps-debian.sh
+++ deps-debian.sh
@@ -14,8 +14,9 @@ asdf plugin-add erlang && asdf plugin-add elixir && asdf plugin-add just
 # Install
 apt-get install -q -y --no-install-recommends autoconf dpkg-dev gcc g++ make libncurses-dev unixodbc-dev libssl-dev libsctp-dev libodbc1 libssl1.1 libsctp1 
 # TODO: use .tool-versions instead of latest
-elixir -v || asdf install erlang latest && asdf global erlang latest && asdf install elixir latest && asdf global elixir latest && asdf global elixir latest #|| apt-get install -y elixir
-just --version || asdf install just latest && asdf global just latest || cargo install just #|| apt-get install -y just 
+which erl || (asdf install erlang latest && asdf global erlang latest)
+elixir -v || (asdf install elixir latest && asdf global elixir latest) #|| apt-get install -y elixir
+just --version || (asdf install just latest && asdf global just latest) || cargo install just #|| apt-get install -y just 
 npm install --global yarn
 
 echo $PATH
