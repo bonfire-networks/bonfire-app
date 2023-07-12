828f74378e8c61308bbe77f92103cf2322139989
Mayel de Borniol <git@mayel.space>
2023-07-12 03:56:46 +0100

    misc

---
+++ flavours/cooperation/repo/migrations/20230304094943_add_type.exs
@@ -0,0 +1,14 @@
+defmodule Bonfire.Repo.Migrations.ClassifyAddType do
+  @moduledoc false
+  use Ecto.Migration
+
+  def up do
+    if Code.ensure_loaded?(Bonfire.Classify.Migrations) do
+      Bonfire.Classify.Migrations.add_type()
+    end
+  end
+
+  def down do
+    # TODO
+  end
+end
