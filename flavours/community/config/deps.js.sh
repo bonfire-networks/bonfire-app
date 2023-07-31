81e259dcb22423721485e2207c0a42a9ff9834b3
Mayel de Borniol <git@mayel.space>
2023-07-31 22:36:57 +0100

    js deps

--- flavours/community/config/deps.js.sh
+++ flavours/community/config/deps.js.sh
@@ -4,7 +4,7 @@
 # NOTE: any LV Hooks should also be added to ./deps_hooks.js
 # TODO: make this more configurable? ie. autogenerate from active extensions with JS assets
 
-DEPS='iconify_ex bonfire_ui_common'
+DEPS='iconify_ex bonfire_ui_common bonfire_editor_milkdown'
 # bonfire_editor_quill bonfire_editor_ck
 
 chmod +x ./js-deps-get.sh
