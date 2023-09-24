0210eb0638092ba7dd9297121fee98dcc00a7c27
Mayel de Borniol <git@mayel.space>
2023-09-24 17:25:41 +0100

    Update .recode.exs

--- .recode.exs
+++ .recode.exs
@@ -1,32 +1,34 @@
-alias Recode.Task
-
 [
-  version: "0.4.0",
-  # Can also be set/reset with "--autocorrect"/"--no-autocorrect".
+  version: "0.6.2",
+  # Can also be set/reset with `--autocorrect`/`--no-autocorrect`.
   autocorrect: true,
   # With "--dry" no changes will be written to the files.
-  # Can also be set/reset with "--dry"/"--no-dry".
+  # Can also be set/reset with `--dry`/`--no-dry`.
   # If dry is true then verbose is also active.
   dry: true,
-  # Can also be set/reset with "--verbose"/"--no-verbose".
+  # Can also be set/reset with `--verbose`/`--no-verbose`.
   verbose: true,
-  # Can be overwriten by calling `mix recode "lib/**/*.ex"`.
-  inputs: [
-    "{flavours,lib,test}/**/*.{ex,exs}",
-    "forks/bonfire*/{config,lib,test}/**/*.{ex,exs}"
-  ],
+  # Can be overwritten by calling `mix recode "lib/**/*.ex"`.
+  inputs: ["{flavours,lib,test}/**/*.{ex,exs}", "forks/bonfire*/{config,lib,test}/**/*.{ex,exs}"],
   formatter: {Recode.Formatter, []},
   tasks: [
     # Tasks could be added by a tuple of the tasks module name and an options
-    # keyword list. A task can be deactived by `active: false`. The execution of
+    # keyword list. A task can be deactivated by `active: false`. The execution of
     # a deactivated task can be forced by calling `mix recode --task ModuleName`.
-    {Task.AliasExpansion, []},
-    {Task.AliasOrder, active: false},
-    {Task.EnforceLineLength, active: false},
-    {Task.PipeFunOne, []},
-    {Task.SinglePipe, active: false}, # Note: does not known how to handle `Arrows`
-    {Task.Specs, active: false, exclude: "{test}/**/*.{ex,exs}", config: [only: :visible]},
-    {Task.TestFileExt, []},
-    {Task.UnusedVariable, active: false}
+    {Recode.Task.AliasExpansion, []},
+    {Recode.Task.AliasOrder, [active: false]},
+    {Recode.Task.Dbg, [autocorrect: false]},
+    {Recode.Task.EnforceLineLength, [active: false]},
+    {Recode.Task.FilterCount, []},
+    {Recode.Task.IOInspect, [autocorrect: false]},
+    {Recode.Task.Nesting, []},
+    {Recode.Task.PipeFunOne, []},
+    {Recode.Task.SinglePipe, [active: false]},
+    {Recode.Task.Specs,
+     [active: false, exclude: "{test}/**/*.{ex,exs}", config: [only: :visible]]},
+    {Recode.Task.TagFIXME, [exit_code: 2]},
+    {Recode.Task.TagTODO, [exit_code: 4]},
+    {Recode.Task.TestFileExt, []},
+    {Recode.Task.UnusedVariable, [active: false]}
   ]
 ]
