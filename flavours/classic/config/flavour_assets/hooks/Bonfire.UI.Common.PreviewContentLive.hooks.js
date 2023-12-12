cf383dab1e820b02857035796611e8d573cf6b77
ivan <ivanminutillo@outlook.it>
2023-12-12 16:07:46 +0100

    improved hook for open the thread

--- flavours/classic/config/flavour_assets/hooks/Bonfire.UI.Common.PreviewContentLive.hooks.js
+++ flavours/classic/config/flavour_assets/hooks/Bonfire.UI.Common.PreviewContentLive.hooks.js
@@ -17,7 +17,7 @@ let PreviewActivity = {
   mounted() {
     this.el.addEventListener("click", e => {
       console.log("PreviewActivity clicked")
-
+      console.log("where clicked")
       let trigger = this.el.querySelector('.open_preview_link')
       let anchor = e.target.closest('a')
 
@@ -25,7 +25,7 @@ let PreviewActivity = {
       // let previewable_activity = e.target.closest('.previewable_activity')
       // anchor == trigger || (!anchor && previewable_activity && ( previewable_activity.classList.contains('previewable_expanded') || this.isTruncated(previewable_activity.querySelector('.previewable_truncate')) == false)
 
-      if (trigger && ((!anchor || anchor.classList.contains('preview_activity_link')) && !e.ctrlKey && !e.metaKey && !window.getSelection().toString() && !e.target.closest('button') && !e.target.closest('figure') && !e.target.closest('.dropdown')
+      if (trigger && ((!anchor || anchor.classList.contains('preview_activity_link')) && !e.ctrlKey && !e.metaKey && !window.getSelection().toString() && !e.target.closest('button') && !e.target.closest('figure') && !e.target.closest('.dropdown') && !e.target.closest('[data-id=activity_actions]')
       )) {
         let uri = this.el.dataset.href || (trigger !== undefined && trigger.getAttribute('href')) //this.el.dataset.permalink
         if (window.liveSocket) {
