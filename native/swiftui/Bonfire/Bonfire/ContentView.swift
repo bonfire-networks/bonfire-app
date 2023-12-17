310c2912d20f260a1563b97973a482badf90324f
Mayel de Borniol <git@mayel.space>
2023-12-17 19:29:42 +0000

    f

--- native/swiftui/Bonfire/Bonfire/ContentView.swift
+++ native/swiftui/Bonfire/Bonfire/ContentView.swift
@@ -3,7 +3,7 @@ import LiveViewNative
 
 struct ContentView: View {
     var body: some View {
-        LiveView(.automatic(URL(string: "https://campground.bonfire.cafe/")!))
-//        LiveView(.automatic(development: .localhost(port: 4000, path: "/"), production: .custom(URL(string: "https://campground.bonfire.cafe/")!)))
+//        LiveView(.automatic(URL(string: "https://campground.bonfire.cafe/")!))
+        LiveView(.automatic(development: .custom(URL(string: "http://localhost:4000/")!), production: .custom(URL(string: "https://campground.bonfire.cafe/")!)))
     }
 }
