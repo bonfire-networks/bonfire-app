9e493e16a995b16c95da6e13134656aa04860b8f
Mayel de Borniol <git@mayel.space>
2023-12-30 20:03:59 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/799

---
+++ flavours/classic/repo/migrations/20200828094946_import_social_graph.exs
@@ -0,0 +1,13 @@
+defmodule Bonfire.Social.Graph.Repo.Migrations.ImportSocial do
+  @moduledoc false
+  use Ecto.Migration
+
+  import Bonfire.Social.Graph.Migrations
+  import Pointers.Migration
+
+  def up do
+    migrate_social()
+  end
+
+  def down, do: migrate_social()
+end
