930eaae8b55826ec5b9104c874b2125549e99623
Mayel de Borniol <git@mayel.space>
2024-02-27 12:48:44 +0000

    ci

--- mix.exs
+++ mix.exs
@@ -110,7 +110,7 @@ defmodule Bonfire.Umbrella.MixProject do
   @default_flavour "classic"
   @config [
     # note that the flavour will automatically be added where the dash appears
-    version: "0.9.10-beta.1",
+    version: "0.9.10-beta.2",
     elixir: ">= #{System.get_env("ELIXIR_VERSION", "1.13.4")}",
     default_flavour: @default_flavour,
     logo: "assets/static/images/bonfire-icon.png",
