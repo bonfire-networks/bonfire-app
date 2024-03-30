b660120438d73a3e61e97ae409dc5ab9a5978b71
Mayel de Borniol <git@mayel.space>
2024-03-30 13:47:15 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/270

--- flavours/classic/config/bonfire_common.exs
+++ flavours/classic/config/bonfire_common.exs
@@ -10,7 +10,16 @@ config :bonfire_common, Bonfire.Common.Localise.Cldr,
   default_locale: default_locale,
   # locales that will be made available on top of those for which gettext localisation files are available
   locales: ["fr", "en", "es"],
-  providers: [Cldr.Language, Cldr.DateTime, Cldr.Number, Cldr.Unit, Cldr.List, Cldr.Calendar],
+  providers: [
+    Cldr.Language,
+    Cldr.DateTime,
+    Cldr.Number,
+    Cldr.Unit,
+    Cldr.List,
+    Cldr.Calendar,
+    Cldr.Territory,
+    Cldr.LocaleDisplay
+  ],
   gettext: Bonfire.Common.Localise.Gettext,
   data_dir: "./priv/cldr",
   add_fallback_locales: true,
