c897e8e4a379fbcfa65ea9bb9d676589cfd7f490
Mayel de Borniol <git@mayel.space>
2024-04-15 23:45:52 +0100

    ci5

--- deps-debian.sh
+++ deps-debian.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
-# NOTE: using asdf because bullseye elixir version is too old
-
-asdf_dir=/opt/asdf
-mkdir -p $asdf_dir
 
 apt-get update -q -y
+
+# deps
 apt-get install -q -y --no-install-recommends git tar file gcc sqlite3 npm mailcap ca-certificates libssl-dev tzdata gettext curl rustc cargo wget gnupg sudo unzip
 
-git clone https://github.com/asdf-vm/asdf.git $asdf_dir --branch v0.11.1
-. "$asdf_dir/asdf.sh"
-asdf plugin-add erlang && asdf plugin-add elixir && asdf plugin-add just
-
-# Install
+# deps of tools
 apt-get install -q -y --no-install-recommends autoconf dpkg-dev gcc g++ make libncurses-dev unixodbc-dev libssl-dev libsctp-dev libodbc1 libssl1.1 libsctp1 
-# TODO: use .tool-versions instead of latest
-which erl || (asdf install erlang latest && asdf global erlang latest)
-elixir -v || (asdf install elixir latest && asdf global elixir latest) #|| apt-get install -y elixir
-just --version || (asdf install just latest && asdf global just latest) || cargo install just #|| apt-get install -y just 
-npm install --global yarn
+
+# NOTE: using mise because bullseye elixir version is too old
+curl https://mise.run | sh
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+mise plugin-add erlang && mise plugin-add elixir && mise plugin-add just
+
+mise install
+
+# FYI: uses .tool-versions instead of the below
+# which erl || (mise install erlang latest && asdf global erlang latest)
+# elixir -v || (asdf install elixir latest && asdf global elixir latest) #|| apt-get install -y elixir
+# just --version || (asdf install just latest && asdf global just latest) || cargo install just #|| apt-get install -y just 
+# npm install --global yarn
 
 echo $PATH
-ls -la $asdf_dir && ls -la $asdf_dir/bin && ls -la /github/home/.asdf/shims
+which erl
 elixir -v
 just --version
 yarn -v
