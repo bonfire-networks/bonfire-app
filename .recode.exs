0654058c6a323c53a9cf7b9e8dd481b7ddce4f27
Simone Robutti <simone.robutti@protonmail.com>
2024-08-01 12:12:25 +0200

    updated version and added linting of extensions

--- .recode.exs
+++ .recode.exs
@@ -1,16 +1,18 @@
 [
-  version: "0.6.2",
+  version: "0.7.2",
   # Can also be set/reset with `--autocorrect`/`--no-autocorrect`.
   autocorrect: true,
   # With "--dry" no changes will be written to the files.
   # Can also be set/reset with `--dry`/`--no-dry`.
   # If dry is true then verbose is also active.
   dry: true,
+  # Enables or disables color in the output.
+  color: true,
   # Can also be set/reset with `--verbose`/`--no-verbose`.
   verbose: true,
   # Can be overwritten by calling `mix recode "lib/**/*.ex"`.
-  inputs: ["{flavours,lib,test}/**/*.{ex,exs}", "forks/bonfire*/{config,lib,test}/**/*.{ex,exs}"],
-  formatter: {Recode.Formatter, []},
+  inputs: ["{flavours,lib,test}/**/*.{ex,exs}", "forks/bonfire*/{config,lib,test}/**/*.{ex,exs}", "extensions/bonfire*/{config,lib,test}/*.{ex,exs}"],
+  formatters: [Recode.CLIFormatter],
   tasks: [
     # Tasks could be added by a tuple of the tasks module name and an options
     # keyword list. A task can be deactivated by `active: false`. The execution of
