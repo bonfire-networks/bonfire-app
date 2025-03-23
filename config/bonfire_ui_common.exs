8a7fb1bad59f35ddb7bd7fcea7f1ac007a8fa5f1
Mayel de Borniol <git@mayel.space>
2025-03-23 17:53:06 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/591 and https://github.com/bonfire-networks/bonfire-app/issues/1151

--- config/bonfire_ui_common.exs
+++ config/bonfire_ui_common.exs
@@ -156,9 +156,9 @@ config :iconify_ex,
   generated_icon_app: :bonfire_ui_common,
   mode: :css,
   using_svg_inject: true,
-  generated_icon_modules_path: "#{dep_path}lib/components/icons",
+  generated_icon_modules_path: "#{dep_path}/lib/components/icons",
   generated_icon_static_url: "/images/icons",
-  generated_icon_static_path: "#{dep_path}assets/static/images/icons"
+  generated_icon_static_path: "#{dep_path}/assets/static/images/icons"
 
 config :surface, :components, [
   {Bonfire.UI.Common.Modular.StatelessComponent, propagate_context_to_slots: true},
