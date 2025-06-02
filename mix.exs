a49782257b98757ccf91e995c814858f814ecc02
Mayel de Borniol <git@mayel.space>
2025-06-02 10:01:01 +0100

    rel

--- mix.exs
+++ mix.exs
@@ -290,7 +290,7 @@ defmodule Bonfire.Umbrella.MixProject do
   # TODO: put these in ENV or an external writeable config file similar to deps.*
   @config [
     # note that the flavour will automatically be added where the dash appears
-    version: "0.9.12-beta.68",
+    version: "0.9.12-beta.69",
     elixir: ">= #{System.get_env("ELIXIR_VERSION", "1.13.4")}",
     flavour: flavour,
     default_flavour: default_flavour,
