a0f65af16fb85a5260cf2fdd949bbbcab789f96e
Mayel de Borniol <git@mayel.space>
2025-08-14 13:15:56 +0100

    optimise compilation

--- config/bonfire_common.exs
+++ config/bonfire_common.exs
@@ -2,14 +2,21 @@ import Config
 
 default_locale = "en"
 
+## Localisation & internationalisation
+# Only compile additional locales in prod or when explicitly requested
+compile_all_locales? = config_env() == :prod or System.get_env("COMPILE_ALL_LOCALES") in ["true", "1"]
+locales = if compile_all_locales?, do: [default_locale, "fr", "es"], else: [default_locale]
+
 config :bonfire_common,
-  otp_app: :bonfire
+  otp_app: :bonfire,
+  localisation_path: "priv/localisation",
+  ecto_repos: [Bonfire.Common.Repo]
 
 # internationalisation
 config :bonfire_common, Bonfire.Common.Localise.Cldr,
   default_locale: default_locale,
   # locales that will be made available on top of those for which gettext localisation files are available
-  locales: ["fr", "en", "es"],
+  locales: locales,
   providers: [
     Cldr.Language,
     Cldr.DateTime,
@@ -21,12 +28,14 @@ config :bonfire_common, Bonfire.Common.Localise.Cldr,
     Cldr.LocaleDisplay
   ],
   gettext: Bonfire.Common.Localise.Gettext,
+  extra_gettext: [Timex.Gettext],
   data_dir: "./priv/cldr",
-  add_fallback_locales: true,
+  add_fallback_locales: compile_all_locales?,
   # precompile_number_formats: ["¤¤#,##0.##"],
   # precompile_transliterations: [{:latn, :arab}, {:thai, :latn}]
   force_locale_download: Mix.env() == :prod,
   generate_docs: true
+
 
 config :ex_cldr_units,
   default_backend: Bonfire.Common.Localise.Cldr
@@ -36,4 +45,15 @@ config :ex_cldr,
   default_backend: Bonfire.Common.Localise.Cldr,
   json_library: Jason
 
+
+config :bonfire_common, Bonfire.Common.Repo,
+  database: System.get_env("POSTGRES_DB", "bonfire_dev"),
+  username: System.get_env("POSTGRES_USER", "postgres"),
+  password: System.get_env("POSTGRES_PASSWORD", "postgres"),
+  # show_sensitive_data_on_connection_error: true,
+  # EctoSparkles does the logging instead
+  log: false,
+  stacktrace: true
+
+  
 config :rustler_precompiled, force_build_all: System.get_env("RUSTLER_BUILD_ALL") in ["true", "1"]
