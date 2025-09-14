f27506d663010b6d303b2628ed675b240196aef1
Mayel de Borniol <git@mayel.space>
2025-09-14 17:00:55 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/1291

--- mix.exs
+++ mix.exs
@@ -217,7 +217,7 @@ defmodule Bonfire.Umbrella.MixProject do
           # git: "https://github.com/germsvel/phoenix_test",
           only: :test, runtime: false
         },
-        {:lazy_html, ">= 0.1.0", only: :test},
+        # {:lazy_html, ">= 0.1.0", only: :test},
         {:patch, "~> 0.15.0"},
         {:mock, "~> 0.3", only: :test},
         {:mox, "~> 1.0", only: [:dev, :test]},
