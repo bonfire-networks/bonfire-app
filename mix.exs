4d2494a29b4ada5303b8f572e6cbbc8f361e481d
Mayel de Borniol <git@mayel.space>
2025-10-06 15:09:49 +0100

    release

--- mix.exs
+++ mix.exs
@@ -64,10 +64,8 @@ defmodule Bonfire.Umbrella.MixProject do
       []
     end
 
-  # TODO: move to ember?
-  with_graphql? = 
-
-  if(System.get_env("WITH_API_GRAPHQL") not in no?,
+  maybe_api_deps =
+    if(System.get_env("WITH_API_GRAPHQL") not in no?,
       do: [
         {:absinthe, "~> 1.7"},
         {:bonfire_api_graphql, git: "https://github.com/bonfire-networks/bonfire_api_graphql"},
@@ -337,7 +335,7 @@ defmodule Bonfire.Umbrella.MixProject do
   # TODO: put these in ENV or an external writeable config file similar to deps.*
   @config [
     # note that the flavour will automatically be added where the dash appears
-    version: "1.0.0-rc.3.3",
+    version: "1.0.0-rc.3.4",
     elixir: ">= #{System.get_env("ELIXIR_VERSION", "1.13.4")}",
     flavour: flavour,
     default_flavour: default_flavour,
