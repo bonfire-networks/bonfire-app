f39c3103d5d5d546e48399787feb9058a17af309
Mayel de Borniol <git@mayel.space>
2026-06-01 13:45:42 +0100

    config

--- config/bonfire_mailer.exs
+++ config/bonfire_mailer.exs
@@ -1,7 +1,7 @@
 import Config
 
 # Our fork of decent with keychain support — precompiled binaries at bonfire-networks/decent
-config :decent, github_url: "https://github.com/bonfire-networks/decent"
+config :decent, github_url: System.get_env("DECENT_GITHUB_URL", "https://github.com/bonfire-networks/decent")
 
 # Force local Rust build when DECENT_BUILD=1
 # (set this when using forks/decent locally or in Docker builds)
