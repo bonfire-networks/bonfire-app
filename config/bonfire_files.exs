8b45f8fc735ae1dc6dcefaca6d5623022a4cdd79
Mayel de Borniol <git@mayel.space>
2026-09-23 16:07:57 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/2325

--- config/bonfire_files.exs
+++ config/bonfire_files.exs
@@ -1,3 +1,10 @@
 import Config
 
 # see `Bonfire.Files.RuntimeConfig` for what env vars to set
+
+# the feed filters this extension owns: the media an activity carries, by type. Declared here as fields on another extension's struct because `Exto` reads this at compile time and a struct's fields have to exist when it compiles; `Bonfire.Files.FeedFilters` is what applies them
+config :bonfire_social, Bonfire.Social.FeedFilters,
+  field: [
+    media_types: Bonfire.Social.FeedFilters.AtomOrStringList,
+    exclude_media_types: Bonfire.Social.FeedFilters.AtomOrStringList
+  ]
--- config/bonfire_tag.exs
+++ config/bonfire_tag.exs
@@ -4,3 +4,10 @@ config :bonfire_tag,
   templates_path: "lib"
 
 config :bonfire_social_graph, skip_boundary_check_types: [Bonfire.Tag.Hashtag]
+
+# the feed filter this extension owns: what a thing is tagged with, by id (which is what a mention is) or by hashtag name. Declared here as a field on another extension's struct because `Exto` reads this at compile time and a struct's fields have to exist when it compiles; `Bonfire.Tag.FeedFilters` is what applies it
+config :bonfire_social, Bonfire.Social.FeedFilters,
+  field: [
+    tags: Bonfire.Social.FeedFilters.StringList,
+    exclude_tags: Bonfire.Social.FeedFilters.StringList
+  ]
